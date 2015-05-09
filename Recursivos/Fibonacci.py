07a4816e0208d86b31337f6e0dd1486c499e62fd
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2015-05-09 12:56:43 -0300

    exclusão de pastas

--- Recursivos/Fibonacci.py
+++
@@ -1,7 +0,0 @@
-def Fibonacci(n):
-	if n <= 1:
-		return 1
-	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
-
-print Fibonacci(4)
--- Recursivos/TorreDeHanoi.py
+++
@@ -1,9 +0,0 @@
-def Hanoi(ori, dst, aux, nro):
-	if nro == 1:
-		print 'Move de ' + str(ori) + ' para ' + str(dst)
-	else:
-		Hanoi(ori, aux, dst, nro-1)
-		Hanoi(ori, dst, aux, 1)
-		Hanoi(aux, dst, ori, nro-1)
-
-Hanoi(0, 2, 1, 6)
