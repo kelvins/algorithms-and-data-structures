dd60f79e3c480afddf1eefc89ad8a525c34de756
Kelvin S. do Prado <kelvinpfw@hotmail.com>
2017-04-06 09:20:38 -0300

    Adicionado selection sort em Go

---
+++ GoLang/selectionsort/selectionsort.go
@@ -0,0 +1,21 @@
+
+package selectionsort
+
+func SelectionSort(slice []int) []int {
+  for index1 := 0; index1 < len(slice)-1; index1++ {
+    indexMin := index1
+
+    for index2 := index1+1; index2 < len(slice); index2++ {
+      if slice[index2] < slice[indexMin] {
+        indexMin = index2
+      }
+    }
+
+    if index1 != indexMin {
+      swap := slice[index1]
+      slice[index1] = slice[indexMin]
+      slice[indexMin] = swap
+    }
+  }
+  return slice
+}
