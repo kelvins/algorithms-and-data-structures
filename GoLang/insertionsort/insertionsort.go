b3d59bb3a12e5d775022cf4ea5e593c8bfe7cbb1
Kelvin S. do Prado <kelvinpfw@hotmail.com>
2017-04-06 09:23:21 -0300

    Adicionado insertion sort em Go

---
+++ GoLang/insertionsort/insertionsort.go
@@ -0,0 +1,17 @@
+
+package insertionsort
+
+func InsertionSort(slice []int) []int {
+  for index1 := 1; index1 < len(slice); index1++ {
+    currentValue := slice[index1]
+    index2 := index1 - 1
+
+    for index2 >= 0 && currentValue < slice[index2] {
+      slice[index2+1] = slice[index2]
+      index2 = index2 - 1
+    }
+
+    slice[index2+1] = currentValue
+  }
+  return slice
+}
