c816284e224ae9182f32a32b8e9bd6028c485cdd
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-04-19 00:19:00 -0300

    Adicionado comentarios ao BubbleSort em Go

--- GoLang/bubbleSort/bubbleSort.go
+++ GoLang/bubbleSort/bubbleSort.go
@@ -1,5 +1,12 @@
+
+/*
+ * Bubble Sort é um dos algoritmos de ordenação mais simples.
+ * A ideia principal é percorrer o vetor/slice várias vezes
+ * empurrando/flutuando os valores mais altos para o fim do vetor.
+ */
 package bubblesort
 
+// Bubble Sort tradicional
 func BubbleSort(slice []int) {
 	for indice1 := len(slice) - 1; indice1 > 0; indice1-- {
 		for indice2 := 0; indice2 < indice1; indice2++ {
@@ -12,8 +19,8 @@ func BubbleSort(slice []int) {
 	}
 }
 
+// Bubble Sort recursivo
 func BubbleSortRecursivo(slice []int, tamanho int) {
-
 	trocas := 0
 	for indice := 0; indice < tamanho-1; indice++ {
 		if slice[indice] > slice[indice+1] {
@@ -23,7 +30,6 @@ func BubbleSortRecursivo(slice []int, tamanho int) {
 			trocas++
 		}
 	}
-
 	if trocas != 0 {
 		BubbleSortRecursivo(slice, tamanho-1)
 	}
