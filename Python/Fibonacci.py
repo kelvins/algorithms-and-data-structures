7b8a270115f66fa1af74fa93c592d22c90ad9020
Kelvin S. do Prado <kelvinpfw@hotmail.com>
2017-10-18 08:39:19 -0200

    Add outra versao Fibonacci

--- Python/Fibonacci.py
+++ Python/Fibonacci.py
@@ -1,7 +1,21 @@
-def Fibonacci(n):
-	if n <= 1:
-		return 1
-	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
 
-print Fibonacci(4)
+def fibonacci1(n):
+    last = 0
+    curr = 1
+    for i in range(0, n):
+        temp = curr
+        curr = curr + last
+        last = temp
+    return last
+
+def fibonacci2(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci2(n-1) + fibonacci2(n-2)
+
+if __name__ == "__main__":
+    print fibonacci1(12)
+    print fibonacci2(12)
