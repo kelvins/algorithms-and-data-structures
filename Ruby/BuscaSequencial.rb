1e035d152ac06c5e2283510764e145e2646b9c74
Élisson Michael <elissonmichael@yahoo.com.br>
2018-08-20 22:44:31 -0300

    Acerta Identação e Nome da Classe de Testes

--- Ruby/BuscaSequencial.rb
+++ Ruby/BuscaSequencial.rb
@@ -1,10 +1,10 @@
 def busca_sequencial(valor, vetor)
-    # Procura o valor no vetor sequencialmente
-    # Se encontrar retorna o índice, senão -1
-    vetor.each_with_index do |elemento, indice|
-      return indice if elemento.equal?(valor)
-    end
-    return -1
+  # Procura o valor no vetor sequencialmente
+  # Se encontrar retorna o índice, senão -1
+  vetor.each_with_index do |elemento, indice|
+    return indice if elemento.equal?(valor)
+  end
+  return -1
 end
 
 # Teste Manual
@@ -14,7 +14,7 @@ puts busca_sequencial(98, vetor)
 # Teste Automatizado
 require 'minitest/autorun'
 
-class Test < MiniTest::Unit::TestCase
+class Test < Minitest::Test
   def test_busca_sequencial_quando_nao_encontra_o_valor
     assert_equal busca_sequencial(5, []), -1
   end
