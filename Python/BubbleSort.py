d6f0809c9d5986a69e09dd5042bcc6775d8ba9ba
kelvins <kelvinpfw@gmail.com>
2019-11-02 10:38:46 -0300

    Corrige indentacao do BubbleSort em Python

--- Python/BubbleSort.py
+++ Python/BubbleSort.py
@@ -1,16 +1,16 @@
 def BubbleSort(vetor, tamanho):
-	trocas = 0
-	for x in xrange(0, tamanho-1):
-		if vetor[x] > vetor[x+1]:
-			temp = vetor[x]
-			vetor[x] = vetor[x+1]
-			vetor[x+1] = temp
-			trocas = 1
-	if trocas != 0:
-		BubbleSort(vetor, tamanho-1)
-	return vetor
+    trocas = 0
+    for x in range(0, tamanho-1):
+        if vetor[x] > vetor[x+1]:
+            vetor[x], vetor[x+1] = vetor[x+1], vetor[x]
+            trocas = 1
+
+    if trocas != 0:
+        BubbleSort(vetor, tamanho-1)
+
+    return vetor
 
 vetor = [2, 9, 8, 0, 1, 3, 5, 4, 6, 7]
-print vetor
+print(vetor)
 vetor = BubbleSort(vetor, len(vetor))
-print vetor
+print(vetor)
