fd45724e66c027a0e6eaff30acfda0d0ed0d8f55
Brayan Arias <stevenson.arias@gmail.com>
2019-11-02 13:23:26 -0500

    se añade main con un ejemplo

--- disjoint_set.java
+++ disjoint_set.java
@@ -2,7 +2,15 @@ Estructura de datos para modelar una colección de conjuntos disyuntos.
 Permite determinar de manera eficiente a que conjunto pertenece un elemento,
 si dos elementos se encuentran en un mismo conjunto y unir dos conjuntos disyuntos en un uno.
 
-static class dsu {
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Estructura de datos para modelar una colección de conjuntos disyuntos.
+// Permite determinar de manera eficiente a que conjunto pertenece un elemento,
+// si dos elementos se encuentran en un mismo conjunto y unir dos conjuntos disyuntos en un uno.
+
+class dsu {
     int[] par, sz;
     int size; //Cantidad de conjuntos
 
@@ -36,3 +44,29 @@ static class dsu {
         return sz[find(u)];
     }
 };
+
+
+class ejemplo{
+    static int n = 10; //cantidad de nodos
+    public static void main(String args[]){
+        dsu ex_dsu = new dsu(n);
+        ex_dsu.unite(1,2);	//une el nodo 1 con el nodo 2
+        ex_dsu.unite(3,4);	//une el nodo 3 con el nodo 4
+        ex_dsu.unite(2,3);	//une el conjunto del nodo 2 con el conjunto del nodo 3
+        ex_dsu.unite(8,5);	//une el nodo 8 con el nodo 5
+        ex_dsu.unite(9,8);	//une el nodo 9 con el conjunto del nodo 8
+        ex_dsu.unite(6,5);	//une el nodo 6 con el conjunto del nodo 5
+		/* Los conjuntos quedarian conformados así
+		[4,3,2,1] y [5,8,9,6]
+		como el nodo 7 y el nodo 0 no se unieron son conjuntos unitarios
+		*/
+        System.out.println("El nodo representativo de 5 es: " + ex_dsu.find(5));
+        System.out.println("El nodo representativo de 1 es: " + ex_dsu.find(1));
+        System.out.println("El nodo representativo de 7 es: " + ex_dsu.find(7));
+        System.out.println("La cantidad de nodos del conjunto donde se encuentra el nodo 5 es: " + ex_dsu.count(5));
+        System.out.println("La cantidad de nodos del conjunto donde se encuentra el nodo 1 es: " + ex_dsu.count(1));
+        System.out.println("La cantidad de nodos del conjunto donde se encuentra el nodo 7 es: " + ex_dsu.count(7));
+        
+
+	}    
+}
