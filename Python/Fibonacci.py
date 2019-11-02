ba1040af7011fdcfc2fde316d9b64072dacd28e3
kelvins <kelvinpfw@gmail.com>
2019-11-02 11:48:00 -0300

    Melhorias no exemplo de Fibonacci em Python

--- Python/Fibonacci.py
+++ Python/Fibonacci.py
@@ -1,21 +1,31 @@
+import functools
 
-def fibonacci1(n):
+
+def fib_iterativo(n):
+    """Fibonnaci iterativo."""
     last = 0
     curr = 1
-    for i in range(0, n):
-        temp = curr
-        curr = curr + last
-        last = temp
+    for _ in range(0, n):
+        last, curr = curr, curr + last
     return last
 
-def fibonacci2(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci2(n-1) + fibonacci2(n-2)
+
+def fib_recursivo(n):
+    """Fibonnaci recursivo."""
+    if n < 2:
+        return n
+    return fib_recursivo(n-1) + fib_recursivo(n-2)
+
+
+@functools.lru_cache(maxsize=None)
+def fib_recursivo_com_cache(n):
+    """Fibonacci recursivo com cache."""
+    if n < 2:
+        return n
+    return fib_recursivo_com_cache(n-1) + fib_recursivo_com_cache(n-2)
+
 
 if __name__ == "__main__":
-    print fibonacci1(12)
-    print fibonacci2(12)
+    print(fib_iterativo(15))
+    print(fib_recursivo(15))
+    print(fib_recursivo_com_cache(15))
