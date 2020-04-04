e2f677f5d15550e267ba46521a2f2506a45ae1ac
Diogo Felipe <dlcfelipp@gmail.com>
2020-04-04 16:11:44 -0300

    Reestruturação da lógica do algoritimo

--- Javascript/BuscaLinear.js
+++ Javascript/BuscaLinear.js
@@ -1,17 +1,11 @@
-function buscaLinear(vetor, valorBuscado, inicio, fim){
-    let posicao = -1;
-
-    if(inicio > fim){
-        return posicao;
-    }
-
-    for(let i = inicio; i < fim; i++){
+function buscaLinear(vetor, valorBuscado){
+    for(let i = 0; i < vetor.length; i++){
         if(vetor[i] == valorBuscado){
-            posicao = i;
+            return i;
         }
     }
-    return posicao;
+    return -1;
 }
 
 var vetor = [54,42,11,33,24,99,77,80];
-console.log(buscaLinear(vetor, 11, 0, vetor.length));
+console.log(buscaLinear(vetor, 11));
