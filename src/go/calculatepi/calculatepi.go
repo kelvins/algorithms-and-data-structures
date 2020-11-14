54f218144da1b36046aea919489fb8524bf08fc2
Heitor <heitorvieira.pvd@gmail.com>
2020-11-13 22:33:15 -0300

    Correção

--- src/go/calculatepi/calculatepi.go
+++ src/go/calculatepi/calculatepi.go
@@ -6,6 +6,8 @@ func CalculatePi(terms []int) []float64 {
 	pi := make([]float64, len(terms))
 
 	for i := 0; i < len(terms); i++ {
+		denominator = 1.0
+		operation = 1.0
 		for cont := 0; cont < terms[i]; cont++ {
 			pi[i] += operation * (4 / denominator)
 			denominator += 2
