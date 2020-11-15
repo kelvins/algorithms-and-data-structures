4a5a07d28e0034f2b3acd48fb3fc96cf19851e4a
Heitor <heitorvieira.pvd@gmail.com>
2020-11-15 10:29:25 -0300

    [rust] adiciona extra palindromo

---
+++ src/rust/src/main.rs
@@ -0,0 +1,13 @@
+mod extra;
+
+fn main() {
+    //Algoritmos
+   
+    //Estruturas de Dados
+
+
+    //Algoritmos de Ordenação
+    
+    //Extra
+    println!("{:?}", extra::palindromo::palindromo("No lemon, no melon".to_string()))
+}
