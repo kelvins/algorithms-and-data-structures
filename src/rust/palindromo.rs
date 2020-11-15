4ab6d7ec1b39fcc5657ade837449db6f9a9d8acc
Heitor <heitorvieira.pvd@gmail.com>
2020-11-15 18:08:14 -0300

    [rust] adiciona algoritmo de Ordenação insertion_sort

--- src/rust/palindromo.rs
+++ src/rust/palindromo.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("{:?}", palindromo("Was it a palindrome?".to_string()));
     println!("{:?}", palindromo("No lemon, no melon".to_string()));
 }
-pub fn palindromo(mut word: String) -> bool {
+fn palindromo(mut word: String) -> bool {
     word = word.to_lowercase().split_whitespace().collect::<String>();
     let reversed_string: String = word.chars().rev().collect::<String>();
     if word.len() <= 1 {
