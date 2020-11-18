0afae4ee3c07f5447233b1e3accebb1e557921b5
Heitor <heitorvieira.pvd@gmail.com>
2020-11-18 14:34:32 -0300

    [rust] adiciona algoritmo fibonacci

---
+++ src/rust/fibonacci.rs
@@ -0,0 +1,13 @@
+fn fibonacci(number: i32) -> i32{
+    if number < 2 {
+      return number;
+    }else{
+      return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+  }
+fn main() {
+    println!("{:?}", fibonacci(30)); 
+    println!("{:?}", fibonacci(50)); 
+    println!("{:?}", fibonacci(3));        
+    println!("{:?}", fibonacci(15));   
+}
