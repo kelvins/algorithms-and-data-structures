9ee149d7db6facec29aa7939c335523de215dda4
Bianca <biancalway@gmail.com>
2020-12-07 14:59:42 -0300

    Algoritmo de busca linear recursiva.

--- src/c/BuscaSequencialRecursiva.c
+++ src/c/BuscaSequencialRecursiva.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-int buscaSequencialRecursiva(int vetor[], int i, int buscado) {
-    size_t n = sizeof(vetor) / sizeof(vetor[0]);
-    if (vetor[i] == buscado)
-    {
-        return i;
-    } 
-    else if (i == n)
+int buscaSequencialRecursiva(int vetor[], int i, int buscado, int size) {
+    
+    if (i == size)
     {
         return -1;
+    } 
+    else if (vetor[i] == buscado)
+    {
+        return i;
     }
     else
     {
-        buscaSequencialRecursiva(vetor, i+1, buscado);
+        buscaSequencialRecursiva(vetor, i+1, buscado, size);
     }
 }
 
 int main(){
-    int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    printf("Valor %d no índice %d\n", 1, buscaSequencialRecursiva(a, 0, 1));
-    buscaSequencialRecursiva(a, 0, 10);
+    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    size_t n = sizeof(vetor) / sizeof(vetor[0]);
+    printf("Valor %d no índice %d\n", 1, buscaSequencialRecursiva(vetor, 0, 1, n));
+    printf("Valor %d no índice %d\n", 10, buscaSequencialRecursiva(vetor, 0, 10, n));
 }
