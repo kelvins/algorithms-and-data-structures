9a5730a0e590b196be9e312aafa69c97dc5f9f41
CarlosViniMSouza <60679101+CarlosViniMSouza@users.noreply.github.com>
2021-04-04 12:44:24 -0300

    arquivo palindromos alterado

--- src/python/palindromos.py
+++ src/python/palindromos.py
@@ -1,28 +1,12 @@
-# 1 - open your terminal and type: pip install pythonds
+# a simplified way to resolve the issue of palindromes.
 
-# 2 - of this package below, download the 'Deque' module 
-from pythonds.basic.deque import Deque
+reading1 = input("insert the word: ")
 
-# to better understand what a 'deck' is about, check out:
-# https://runestone.academy/runestone/books/published/pythonds/BasicDS/WhatIsaDeque.html
+# reading2 receives the variable (only the other way around)
+reading2 = reading1[::-1] 
 
-# 3 - the function will tell you if that word is palindrome
-def checking(reading2ring):
-    character_deque = Deque()
-    for word in reading2ring:
-        character_deque.addRear(word)
-    equity = 'right'
-
-# 4 - the 'while' loop will read the information placed
-    while (character_deque.size() > 1 and equity):
-        reading1 = character_deque.removeFront() # add the element at the rear
-        reading2 = character_deque.removeRear() # removes the element at the rear
-
-# 5 - the 'if' loop will give the 'wrong' answer
-        if (reading1 != reading2):
-            equity = 'wrong'
-    return equity
-
-# 6 - check the result - always invoking the function 'checking ()'
-print(checking("hannah")) # example1 : right
-print(checking("abcabc")) # example2 : wrong
+# return True or False (depending on the input value)
+if (reading1 == reading2):
+    print(True)
+else:
+    print(False)
