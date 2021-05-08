a640637911c5329120606e5417f05602cb70f47e
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-07 22:57:22 -0300

    Adicionando cálculo de pi em c

---
+++ src/c/CalculatePi.c
@@ -0,0 +1,30 @@
+/*
+Calculo de Pi em C
+Bruno Dantas de Paiva - 2021
+https://github.com/DantasB
+*/
+
+#include <stdio.h>
+
+float leibniz_pi_calculation(int number)
+{
+    float denominador = 1.0f;
+    float operacao = 1.0f;
+    float pi = 0.0f;
+    for (int i = 0; i < number; i++)
+    {
+        pi += operacao * (4.0 / denominador);
+        denominador += 2.0;
+        operacao = operacao * -1.0;
+    }
+    return pi;
+}
+
+int main()
+{
+    int n_terms[4] = {10, 1000, 100000, 10000000};
+    for (int n = 0; n < 4; n++)
+        printf("PI (%i): {%f}\n", n, leibniz_pi_calculation(n_terms[n]));
+
+    return 0;
+}
