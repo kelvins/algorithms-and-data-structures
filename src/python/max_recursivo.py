f695efbad744a962188ef4f3bd1bed9640efe9bf
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-13 10:48:57 -0300

    Minor Changes

--- src/python/max_recursivo.py
+++ src/python/max_recursivo.py
@@ -4,7 +4,6 @@
 # https://github.com/DantasB
 
 import random
-from functools import reduce
 
 # Maximo usando Divisão e Conquista
 
@@ -21,26 +20,20 @@ def max_dc(vetor, left, right):
     return max_1 if max_1 > max_2 else max_2
 
 
-def max_1(vetor, maximo, indice):
-    if vetor[indice] > maximo:
-        maximo = vetor[indice]
+def max_recursive(vetor, maximo, indice) -> int:
+    result = maximo
+    if vetor[indice] > result:
+        result = vetor[indice]
 
     if indice < len(vetor) - 1:
-        max_1(vetor, maximo, indice+1)
+        max_recursive(vetor, result, indice+1)
     else:
-        print(f"Max1: {maximo}")
+        return result
 
 
 if __name__ == "__main__":
-    vetor = [0 for _ in range(0, 10)]
-
-    for i in range(len(vetor)):
-        vetor[i] = random.randrange(10, 100)
+    vetor = [random.randrange(10, 100) for _ in range(0, 10)]
 
     print(vetor)
-
-    max_1(vetor, vetor[0], 1)
-
+    print(f"Max Recursive: {max_recursive(vetor, vetor[0], 1)}")
     print(f"Max DC: {max_dc(vetor, 0, len(vetor)-1)}")
-
-    print(f"Max Lambda: {max_dc(vetor, 0, len(vetor)-1)}")
