a5a669e1dfefdd5ef7d1b86fdf0d62c4afd95c6c
Poskvansk <luisfilipesrct@gmail.com>
2021-12-22 12:07:07 -0300

    add counting sort

---
+++ src/cpp/MaximoRecursivo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+
+int max_recursivo(std::vector<int> nums, int n, int max) {
+
+    if(n == 1)
+        return nums[0];
+
+    
+
+
+    
+}
+
+int main() {
+
+    std::vector<int> nums{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    // std::cout << max_recursivo(nums, nums.size()) << std::endl;
+
+    return 0;
+}
