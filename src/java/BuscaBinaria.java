bd60db1889d3375cd7039b953630e5748db69ce8
Kelvin S. do Prado <kelvinpfw@gmail.com>
2022-04-21 11:30:07 -0300

    Update BuscaBinaria.java

--- src/java/BuscaBinaria.java
+++ src/java/BuscaBinaria.java
@@ -1,30 +1,28 @@
-
 public class BuscaBinaria {
 
-	public int posicao;
-	
-	BuscaBinaria(int[] array, int Objetivo){
-		this.posicao = BinarySearch(Objetivo, array);
-	}
+    public int posicao;
 
-	public int GetResultado() {
-		return this.posicao;
-	}
+    BuscaBinaria(int[] array, int Objetivo) {
+        this.posicao = BinarySearch(Objetivo, array);
+    }
 
-	public int BinarySearch(int objetivo, int[] array) {
-		int comeco = 0;
-		int fim = array.length - 1; 
-        while (comeco <= fim) { 
-            int meio = comeco + (fim-comeco)/2; 
-            if (array[meio] == objetivo) 
-                return meio; 
-            if (array[meio] < objetivo) 
-                comeco = meio + 1; 
+    public int GetResultado() {
+        return this.posicao;
+    }
+
+    public int BinarySearch(int objetivo, int[] array) {
+        int comeco = 0;
+        int fim = array.length - 1;
+
+        while (comeco <= fim) {
+            int meio = comeco + (fim - comeco) / 2;
+            if (array[meio] == objetivo)
+                return meio;
+            if (array[meio] < objetivo)
+                comeco = meio + 1;
             else
-            	fim = meio - 1; 
+                fim = meio - 1;
         }
-        return -1; 
+        return -1;
     }
 }
-
-
