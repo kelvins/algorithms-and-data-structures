b47a407411a5a7af4a2dd6e5b109e1a26f971bdb
Kelvin S. do Prado <kelvinpfw@gmail.com>
2022-04-23 17:09:21 -0300

    Update fatorial_recursiva.rs

--- src/rust/fatorial_recursiva.rs
+++ src/rust/fatorial_recursiva.rs
@@ -1,6 +1,6 @@
 /*
     Contribuidores
-		- Heitor582 
+        - Heitor582 
         - Dromedario de Chapéu
 
     Fatorial é uma função matematica que consistem em realizar
@@ -12,8 +12,8 @@
 // Para realizar uma fatoração com recursão basta fazer o retorno
 // de uma função ser valor * a propia função recebendo valor - 1 
 fn fatorial(valor: u128) -> u128 {
-	// Para que não chege a multiplicar por 0 quando chegamos a 1 ou 0 é 
-	// retornado 1 para que o utlimo valor não sejá zerado ao multiplicar por zero
+    // Para que não chege a multiplicar por 0 quando chegamos a 1 ou 0 é 
+    // retornado 1 para que o utlimo valor não sejá zerado ao multiplicar por zero
     match valor {
         0 | 1  => 1,
         2.. => valor * (fatorial(valor - 1)),
