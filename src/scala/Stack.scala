e2d38d06aa3c48d36ed68f10009ec86f7f608b9b
Kelvin S. do Prado <kelvinpfw@gmail.com>
2022-04-24 10:26:50 -0300

    Add stack em scala

---
+++ src/scala/Stack.scala
@@ -0,0 +1,32 @@
+// Note that this is just for study purposes since Scala provides a built-in Stack collection:
+// https://www.scala-lang.org/api/current/scala/collection/mutable/Stack.html
+
+import scala.collection.mutable.ListBuffer
+
+class Stack[Any] {
+  private var stack: ListBuffer[Any] = ListBuffer()
+
+  def push(value: Any): Unit = {
+    stack += value
+  }
+
+  def pop(): Any = {
+    stack.remove(stack.length - 1)
+  }
+
+  override def toString(): String = stack.mkString(", ")
+}
+
+object Main extends App {
+  val stack = new Stack[Int]
+
+  println("Pushing 3 values...")
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  println(stack)
+
+  println("Poping 1 value...")
+  stack.pop()
+  println(stack)
+}
