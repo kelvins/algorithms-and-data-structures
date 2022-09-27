8248ae4ad499509365fa0c78dac3f1db30f5e63f
Alfredo <alfredo.alfpaes@gmail.com>
2022-09-26 23:24:18 -0300

    Adicionado no diretório kotlin, os algoritmos para Exponenciação Recursiva, Fatorial e Fibonacci.

---
+++ src/kotlin/ExponenciacaoRecursiva.kt
@@ -0,0 +1,23 @@
+/**
+ * Função "exponenciacaoRecusriva"
+ * 
+ * A função *exponenciacaoRecusriva* apresenta os valores de um número(*base*) elevado por um *expoente*.
+ * Nessa função, usa -se o conceito da *recursividade*, na qual, a função criada é chamada dentro dela, 
+ * uma ou mais vezes internamente da mesma.
+ * 
+ * @author Versão do algoritmo para Kotlin: Alfredo Paes <alfredo.alfpaes@gmail.com>
+ * @see https://github.com/Alfredo-Paes
+ * 
+ * @param base é do tipo inteiro(Int)
+ * @param expoente é do tipo inteiro(Int) 
+ * 
+ * @return retornará o número *base* elevado pelo *expoente*. A função retorna um valor do tipo *Long*.
+ */
+fun exponenciacaoRecusriva(base: Int, expoente: Int): Long {
+  return if (expoente === 0) 1;
+  else base * exponenciacaoRecusriva(base, expoente - 1);
+}
+
+fun main() {
+ println(exponenciacaoRecusriva(2, 3));
+}
