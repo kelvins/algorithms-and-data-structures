f7d8bc9d38b094dc8fcc75f40300b19ee582a8dd
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-04-21 23:20:43 -0300

    Apply ktlint format

--- src/kotlin/Fatorial.kt
+++ src/kotlin/Fatorial.kt
@@ -1,27 +1,27 @@
 /**
  * Função "fatorial"
- * 
+ *
  * A função *Fatorial* apresenta os valores da multiplicação *n números* por seus antecessores maiores ou iguais a 1.
- * 
+ *
  * @author Versão do algoritmo para Kotlin: Alfredo Paes <alfredo.alfpaes@gmail.com>
  * @see https://github.com/Alfredo-Paes
- * 
+ *
  * @param numero é do tipo inteiro(Int)
- * 
+ *
  * @return retornará um número do tipo *Long* no qual o tipo está atribuido para a variável *fatorial*.
  */
 
-fun fatorial (numero: Int) {
-  val numeroInicial: Int = numero;
-  var fatorial: Long = 1;
+fun fatorial(numero: Int) {
+    val numeroInicial: Int = numero
+    var fatorial: Long = 1
 
-  for(i in 1..numeroInicial) {
-      fatorial *= i.toLong();
-  }
+    for (i in 1..numeroInicial) {
+        fatorial *= i.toLong()
+    }
 
-  println("Fatorial de $numero! é $fatorial");
+    println("Fatorial de $numero! é $fatorial")
 }
 
 fun main() {
-  fatorial(7);
+    fatorial(7)
 }
