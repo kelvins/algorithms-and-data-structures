7674e3f4d18f23592bc81f2ed97f287f9681b0fa
ricardocarva <ricardofecarva@gmail.com>
2023-09-14 11:54:09 -0400

    Added Algorithm for TwoSum.c and updatedthe README.md

---
+++ src/c/TwoSum.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+twoSum Algorithm in C
+Ricardo Ferreira Carvalheira - 2023
+https://github.com/ricardocarva
+
+	Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+	
+	You may assume that each input would have exactly one solution, and you may not use the same element twice.
+	
+	You can return the answer in any order.
+*/
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+   
+    for(int i=0;i<numsSize-1;i++)
+    {
+        for( int j=i+1;j<numsSize;j++)
+        {
+            if(nums[i]+nums[j]==target)
+            {
+                
+                int * x =(int*) malloc(2*sizeof(int));
+                *returnSize=2;
+                x[0]=i;
+                x[1]=j;
+                return x;
+
+            }    
+        }
+    }
+    return NULL;
+
+}
+
+int main() {
+    int nums[] = {2, 11, 7, 15};
+    int target = 9;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize = 0;
+    
+    printf("The target number is %d.", target);
+    printf("The list has the following items: ");
+    
+    for(int i=0; i< numsSize; i++)
+    {
+    	printf("%d ", nums[i]);
+	}
+    
+    printf("\n");
+    
+    int* result = twoSum(nums, numsSize, target, &returnSize);
+    
+    if (result != NULL) {
+    	printf("The two numbers that add up to %d are: %d and %d\n", target, nums[0], nums[1]);
+        printf("Indices of the two numbers that add up to %d are: %d and %d\n", target, result[0], result[1]);
+        free(result); // Don't forget to free the allocated memory
+    } else {
+        printf("No solution found.\n");
+    }
+    
+    return 0;
+}
