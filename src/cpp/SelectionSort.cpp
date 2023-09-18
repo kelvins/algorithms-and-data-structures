665385e06b6ac6f6e155fbcfef306529571fe36e
ricardocarva <ricardofecarva@gmail.com>
2023-09-18 12:10:29 -0400

    Translated SelectionSort to English

--- src/cpp/SelectionSort.cpp
+++ src/cpp/SelectionSort.cpp
@@ -3,46 +3,46 @@
 
 using namespace std;
 
-void selectionSort(vector<int> &vetor)
+void selectionSort(vector<int> &vector)
 {
-    for (uint32_t indice = 0; indice < vetor.size()-1; indice++)
+    for (uint32_t index = 0; index < vector.size()-1; index++)
     {
-        uint32_t min = indice;
+        uint32_t min = index;
         
-        for (uint32_t tempIndice = indice+1; tempIndice < vetor.size(); ++tempIndice)
+        for (uint32_t tempIndex = index+1; tempIndex < vector.size(); ++tempIndex)
         {
-            if( vetor[tempIndice] < vetor[min] )
+            if( vector[tempIndex] < vector[min] )
             {
-                min = tempIndice;
+                min = tempIndex;
             }
         }
         
-        if( min != indice )
+        if( min != index )
         {
-            int temp = vetor[indice];
-            vetor[indice] = vetor[min];
-            vetor[min] = temp;
+            int temp = vector[index];
+            vector[index] = vector[min];
+            vector[min] = temp;
         }
     }
 }
 
-void showVector(vector<int> vetor)
+void showVector(vector<int> vector)
 {
-    for (uint32_t i = 0; i < vetor.size(); ++i)
+    for (uint32_t i = 0; i < vector.size(); ++i)
     {
-        cout << vetor[i] << ", ";
+        cout << vector[i] << ", ";
     }
     cout << "\n";
 }
 
 int main()
 {
-    vector<int> vetor;
+    vector<int> vector;
     for (uint32_t i = 0; i < 10; ++i)
     {
-        vetor.push_back(rand() % 100);
+        vector.push_back(rand() % 100);
     }
-    showVector(vetor);
-    selectionSort(vetor);
-    showVector(vetor);
+    showVector(vector);
+    selectionSort(vector);
+    showVector(vector);
 }
