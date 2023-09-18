04b82546f766c90b4aed5f408a7ea884a7a3df5c
ricardocarva <ricardofecarva@gmail.com>
2023-09-18 12:23:29 -0400

    Translated FactorialRecursive cpp

--- src/cpp/FatorialRecursiva.cpp
+++ src/cpp/FatorialRecursiva.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int fatorial(int n) {
+int factorial(int n) {
 
     if(n == 0) 
         return 1;
 
     else
-        return n * (fatorial(n-1));
+        return n * (factorial(n-1));
 
 }
 
@@ -15,7 +15,7 @@ int main() {
     int nums[] = {0, 1, 2, 3, 4, 5};
 
     for(auto i : nums) {
-        std::cout << std::to_string(i)+"! = " << fatorial(i) << std::endl; 
+        std::cout << std::to_string(i)+"! = " << factorial(i) << std::endl; 
     }
 
     return 0;
