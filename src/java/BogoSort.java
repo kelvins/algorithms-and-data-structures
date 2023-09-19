9bdfb928375923b9a03f2a34084221fb0a7877ce
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-09-19 20:19:20 -0300

    Update BogoSort.java

--- src/java/BogoSort.java
+++ src/java/BogoSort.java
@@ -1,14 +1,10 @@
 /*
- *
  * @author Marcelo Wischniowski <marcelowisc at gmail.com>
  */
 public class BogoSort {
 
-  /**
-   * @param vector
-   */
-  public int[] sort(int[] vector) {
-    // Checking if the vector is ordered
+  public static int[] bogoSort(int[] vector) {
+    // Check if the vector is ordered
     while (!isSorted(vector)) {
       // Shuffle the vector to try to sort it randomly
       shuffle(vector);
@@ -17,10 +13,7 @@ public class BogoSort {
   }
 
   /**
-   * Checking again if the vector is ordered
-   *
-   * @param vector
-   * @return
+   * Check if the vector is ordered
    */
   private static boolean isSorted(int[] vector) {
     for (int i = 0; i < (vector.length - 1); ++i) {
@@ -34,8 +27,6 @@ public class BogoSort {
 
   /**
    * Shuffle the vector to try to sort it randomly
-   *
-   * @param vector Vector is to be sorted
    */
   private static void shuffle(int[] vector) {
     for (int x = 0; x < vector.length; ++x) {
@@ -46,53 +37,25 @@ public class BogoSort {
       vector[index2] = a;
     }
   }
-}
-/*
- *
- * @author Marcelo Wischniowski <marcelowisc at gmail.com>
- */
-public class BogoSort {
 
-  /**
-   * @param vector
-   */
-  public int[] sort(int[] vector) {
-    // Verify if the vector is ordered
-    while (!isSorted(vector)) {
-      // Shuffle the vector to try to sort it randomly
-      shuffle(vector);
+  public static void printVector(int vector[]) {
+    for (int i = 0; i < vector.length; i++) {
+      System.out.print(vector[i] + ", ");
     }
-    return vector;
+    System.out.println("");
   }
 
-  /**
-   * Verify if the vector (named vector) is sorted 
-   *
-   * @param vector
-   * @return
-   */
-  private static boolean isSorted(int[] vector) {
-    for (int i = 0; i < (vector.length - 1); ++i) {
-      if (vector[i] > vector[i + 1]) {
-        return false;
-      }
-    }
+  public static void main(String[] args) {
 
-    return true;
-  }
+    int vector[] = {9, 0, 4, 2, 3, 8, 7, 1, 6, 5};
 
-  /**
-   * Shuffle the vector to try to sort it randomly
-   *
-   * @param vector Vector is to be sorted
-   */
-  private static void shuffle(int[] vector) {
-    for (int x = 0; x < vector.length; ++x) {
-      int index1 = (int) (Math.random() * vector.length),
-          index2 = (int) (Math.random() * vector.length);
-      int a = vector[index1];
-      vector[index1] = vector[index2];
-      vector[index2] = a;
-    }
+    System.out.println("BogoSort:");
+    System.out.println("Unordered vector:");
+    printVector(vector);
+
+    vector = bogoSort(vector);
+
+    System.out.println("Sorted vector:");
+    printVector(vector);
   }
 }
