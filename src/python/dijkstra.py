247f8f9dc8f25636f80d54f0e0fb4cb0c2fc9680
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-09-21 03:51:04 +0530

    dijkstra

--- src/python/dijkstra.py
+++ src/python/dijkstra.py
@@ -6,11 +6,11 @@ import heapq
 
 
 class Grafo:
-    """Define um grafo utilizando matriz de adjacências.
+    """Define a graph using an adjacency matrix.
 
     Args:
-        arestas (list): uma lista de listas onde o indice é o
-                        vértice e cada elemento da lista é o vizinho
+        Edges (list): a list of lists where the index is the
+                        vertex, and each element of the list is the neighbor.
     """
 
     def __init__(self, arestas: list):
@@ -19,50 +19,50 @@ class Grafo:
         self.adiciona_arestas(arestas)
 
     def adiciona_arestas(self, arestas: list) -> None:
-        """Adiciona todas as arestas ao grafo
+        """  Add all the edges to the grafo
 
         Args:
-            arestas (list): a lista contendo todas as definições de arestas do grafo
+            Edges (list): the list containing all the edge definitions of the graph.
         """
         for i in range(len(arestas)):
             for j in range(len(arestas[i])):
                 self.__adiciona_aresta(i, arestas[i][j])
 
     def __adiciona_aresta(self, u: int, v: int) -> None:
-        """Adiciona a aresta na matriz de adjacência
+        """Add the edge to the adjacency matrix.
 
         Args:
-            u (int): vértice u
-            v (int): vértice v
+            u (int): vertex u
+            v (int): vertex v
         """
         if v[0] not in self.adj[u]:
             self.adj[u].append([v[1], v[0]])
 
     def _peso_entre_u_e_v(self, u: int, v: int) -> float:
-        """Retorna o peso entre os vértices u e v
+        """Return the weight between vertices u and v.
 
         Args:
-            u (int): vértice u
-            v (int): vértice v
+            u (int): vertex u
+            v (int): vertex v
 
         Returns:
-            float: peso entre u e v
+            float: weight between u and v
         """
         for vertice in self.adj[v[1]]:
             if vertice[1] == u:
                 return vertice[0]
 
     def dijkstra(self, start: int) -> list:
-        """Retorna a lista de distância do vértice start até todos os vértices
+        """Returns the list of distances from the start vertex to all vertices.
 
         Args:
-            start (int): vértice inicial
+            start (int): initial vertex
 
         Returns:
-            list: lista de distâncias
+            list: list of distances
         """
         distancia = self.dist.copy()
-        S = set()  # Conjunto de vértices explorados
+        S = set()  # Set of explored vertices
         distancia[start] = 0
 
         while True:
@@ -83,12 +83,13 @@ class Grafo:
 
 
 arestas = [
-    [[1, 1], [2, 0.3], [5, 0.2]],  # Vizinhos do vértice 0.
-    [[0, 1], [2, 0.5]],  # Vizinhos do vértice 1.
-    [[0, 0.3], [1, 0.5], [3, 1.5], [4, 2]],  # Vizinhos do vértice 2.
-    [[2, 1.5], [4, 1.3], [5, 0.8]],  # Vizinhos do vértice 3.
-    [[2, 2], [3, 1.3]],  # Vizinhos do vértice 4.
-    [[0, 0.2], [3, 0.8]],  # Vizinhos do vértice 5.
+    [[1, 1], [2, 0.3], [5, 0.2]],  # Neighbors of vertex 0.
+    [[0, 1], [2, 0.5]],  # Neighbors of vertex 1.
+    [[0, 0.3], [1, 0.5], [3, 1.5], [4, 2]],  # Neighbors of vertex 2.
+    [[2, 1.5], [4, 1.3], [5, 0.8]],  # Neighbors of vertex 3.
+    [[2, 2], [3, 1.3]],  # Neighbors of vertex 4.
+    [[0, 0.2], [3, 0.8]],  # Neighbors of vertex 5.
+
 ]
 
 grafo = Grafo(arestas)
