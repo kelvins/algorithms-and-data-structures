41aad629ab54c086709298875adc7b57e0d1cfec
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-09-21 02:09:00 +0530

    Sentinal Search

--- src/python/busca_sentinela.py
+++ src/python/busca_sentinela.py
@@ -1,15 +1,14 @@
-""" Implementacao do algoritmo de busca sentinela """
+""" Implementation of the sentinel search algorithm """
 
-
-def busca_sentinela(list_to_search, value):
+def sentinel_search(list_to_search, value):
     """
-    Implementacao de um algoritmo de busca sentinela.
+    Implementation of a sentinel search algorithm.
 
-    Argumentos:
-    value: Any. Valor a ser buscado na lista
-    list_to_search: list. lista na qual o valor sera buscado
+    Arguments:
+    value: Any. The value to be searched in the list.
+    list_to_search: list. The list in which the value will be searched.
 
-    Retorna o indice do valor em "list_to_search" ou -1 caso nao exista nela.
+    Returns the index of the value in "list_to_search" or -1 if it does not exist in the list.
     """
     list_to_search.append(value)
     list_index = 0
@@ -20,11 +19,10 @@ def busca_sentinela(list_to_search, value):
         return -1
     return list_index
 
-
 if __name__ == "__main__":
     some_list = [1, 9, 2, 8, 7, 4, 5, 6, 4, 3, 10, 0]
     NUMBER_TO_FIND = 4
-    NUMBER_INDEX = busca_sentinela(some_list, NUMBER_TO_FIND)
+    NUMBER_INDEX = sentinel_search(some_list, NUMBER_TO_FIND)
     print(some_list)
     if NUMBER_INDEX >= 0:
         print("Found value {} at position {}.".format(NUMBER_TO_FIND, NUMBER_INDEX))
