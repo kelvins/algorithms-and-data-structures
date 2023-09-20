36948ad89b57bcb75dc108fa2325667dc615bd5e
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-09-21 03:38:07 +0530

    Counting Sort

--- src/python/counting_sort.py
+++ src/python/counting_sort.py
@@ -1,45 +1,43 @@
-# Algoritmo de ordenação Counting sort em Python
+# Counting sort algorithm in Python
 # Vinicios Barbosa da Silva - 2023
 # https://github.com/ViniciosB
 
 import random
 
-
 def counting_sort(arr):
-    # Encontra o maior elemento na lista
+    # Find the maximum element in the list
     k = max(arr) + 1
 
-    # Inicializa o array de contagem com zeros
+    # Initialize the counting array with zeros
     count = [0] * k
 
-    # Conta a frequência de cada elemento
+    # Count the frequency of each element
     for i in arr:
         count[i] += 1
 
-    # Atualiza o array de contagem para refletir a posição correta de cada elemento na lista ordenada
+    # Update the counting array to reflect the correct position of each element in the sorted list
     for i in range(1, k):
         count[i] += count[i - 1]
 
-    # Inicializa o array de resultado com zeros
+    # Initialize the result array with zeros
     result = [0] * len(arr)
 
-    # Preenche o array de resultado com os elementos ordenados
+    # Fill the result array with the sorted elements
     for i in reversed(arr):
         result[count[i] - 1] = i
         count[i] -= 1
 
     return result
 
-
-# Gera uma lista de n números aleatórios
+# Generate a list of n random numbers
 n = 10
 lista = [random.randint(0, 100) for _ in range(n)]
 
-# Imprime a lista original sem ordenação
-print(f"Lista Original: {lista}")
+# Print the original unsorted list
+print(f"Original List: {lista}")
 
-# Ordena a lista utilizando o algoritmo de Counting Sort
-lista_ordenada = counting_sort(lista)
+# Sort the list using the Counting Sort algorithm
+sorted_list = counting_sort(lista)
 
-# Imprime a lista ordenada
-print(f"Lista Ordenada: {lista_ordenada}")
+# Print the sorted list
+print(f"Sorted List: {sorted_list}")
