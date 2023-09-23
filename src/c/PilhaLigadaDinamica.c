8fed10c66604e521cba686a4e6606325545a6b26
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:15:47 +0330

    Update PilhaLigadaDinamica.c

--- src/c/PilhaLigadaDinamica.c
+++ src/c/PilhaLigadaDinamica.c
@@ -1,83 +1,74 @@
-/*
-*	Pilha Dinâmica utilizando uma Lista Ligada em C
-*	Kelvin Salton do Prado - 2015
-*/
-
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <stdbool.h> // Include for the 'bool' data type
 
 #define ERRO -1
 
 typedef int TIPOCHAVE;
 
-typedef struct AUX{
-	TIPOCHAVE chave;
-	struct AUX *prox;
-}*PILHA;
+typedef struct AUX {
+    TIPOCHAVE chave;
+    struct AUX* prox;
+} *PILHA;
 
-PILHA CREATE(TIPOCHAVE ch){
-	PILHA novaPilha = (PILHA) malloc( sizeof(PILHA) );
-	novaPilha->chave = ch;
-	novaPilha->prox = NULL;
-	return novaPilha;
+PILHA CREATE(TIPOCHAVE ch) {
+    PILHA novaPilha = (PILHA)malloc(sizeof(PILHA));
+    novaPilha->chave = ch;
+    novaPilha->prox = NULL;
+    return novaPilha;
 }
 
-PILHA PUSH(TIPOCHAVE ch, PILHA pi){
-	/*while( pi->prox != NULL ){
-		pi = pi->prox;
-	}*/
-	PILHA novo = CREATE(ch);
-	//pi->prox = novo;
-	novo->prox = pi;
-	return novo;
+PILHA PUSH(TIPOCHAVE ch, PILHA pi) {
+    PILHA novo = CREATE(ch);
+    novo->prox = pi;
+    return novo;
 }
 
-PILHA POP(PILHA pi){
-	PILHA sub = pi->prox;
-	free(pi);
-	return sub;
+PILHA POP(PILHA pi) {
+    PILHA sub = pi->prox;
+    free(pi);
+    return sub;
 }
 
-void SHOW(PILHA pi){
-	printf("PILHA:\n");
-	while( pi->prox != NULL ){
-		printf("[ %d ]\n", pi->chave);
-		pi = pi->prox;
-	}
-	printf("[ %d ]\n", pi->chave);
+void SHOW(PILHA pi) {
+    printf("PILHA:\n");
+    while (pi != NULL) {
+        printf("[ %d ]\n", pi->chave);
+        pi = pi->prox;
+    }
 }
 
-bool SEARCH(TIPOCHAVE ch, PILHA pi){
-	bool vAchou = false;
-	while(pi != NULL){
-		if( pi->chave == ch )
-			vAchou = true;
-		pi = pi->prox;
-	}
-	return vAchou;
+bool SEARCH(TIPOCHAVE ch, PILHA pi) {
+    bool vAchou = false;
+    while (pi != NULL) {
+        if (pi->chave == ch)
+            vAchou = true;
+        pi = pi->prox;
+    }
+    return vAchou;
 }
 
-int main(){
-	PILHA vPilha; 
-	
-	vPilha = PUSH(1, vPilha);
-	vPilha = PUSH(2, vPilha);
-	vPilha = PUSH(3, vPilha);
-	vPilha = PUSH(4, vPilha);
-	vPilha = PUSH(5, vPilha);
-	vPilha = PUSH(6, vPilha);
+int main() {
+    PILHA vPilha = NULL; // Initialize the stack pointer as NULL
 
-	SHOW(vPilha);
-	
-	vPilha = POP(vPilha);
-	vPilha = POP(vPilha);
+    vPilha = PUSH(1, vPilha);
+    vPilha = PUSH(2, vPilha);
+    vPilha = PUSH(3, vPilha);
+    vPilha = PUSH(4, vPilha);
+    vPilha = PUSH(5, vPilha);
+    vPilha = PUSH(6, vPilha);
 
-	SHOW(vPilha);
+    SHOW(vPilha);
 
-	if( SEARCH(6, vPilha) )
-		printf("\nAchou\n");
-	else
-		printf("\nNão achou\n");
+    vPilha = POP(vPilha);
+    vPilha = POP(vPilha);
 
-	return 0;
+    SHOW(vPilha);
+
+    if (SEARCH(6, vPilha))
+        printf("\nAchou\n");
+    else
+        printf("\nNão achou\n");
+
+    return 0;
 }
