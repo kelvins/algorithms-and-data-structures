fb1325a57a8d2c4a7f34a56466bd3c55465c2eff
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:05:52 +0330

    Update ComponentesConexos.c

--- src/c/ComponentesConexos.c
+++ src/c/ComponentesConexos.c
@@ -1,9 +1,9 @@
 /*
 *
-*	Grafos - Algoritmo para calcular o número de componentes conexos em um determinado Grafo
+*	Graphs - Algorithm to calculate the number of connected components in a given Graph
 *	Kelvin Salton do Prado - 2015
 *
-*	GRAFO
+*	GRAPH
 *   (0)               (1)-------------(4)---------------(5)
 *    |                 |               |                 |
 *    |                 |               |                 |
@@ -13,7 +13,7 @@
 *                      -----------------------------------
 *
 *
-*    Matriz de Adjacência
+*    Adjacency Matrix
 *       0  1  2  3  4  5 
 *    0  0  -  1  -  -  -
 *    1  -  0  -  1  1  -
@@ -23,8 +23,8 @@
 *	 5  -  -  -  1  1  0
 *
 *
-*	6 Vértices
-*	8 Arestas
+*	6 Vertices
+*	8 Edges
 */
 
 #include <stdio.h>
@@ -32,38 +32,38 @@
 #define VERTICES 6
 #define INF -1
 
-bool visitados[VERTICES];
-int componentes = 0;
+bool visited[VERTICES];
+int components = 0;
 
-int matriz[VERTICES][VERTICES]  = { {   0, INF,   1, INF, INF, INF },
-									{ INF,   0, INF,   1,   1, INF },
-									{   1, INF,   0, INF, INF, INF },
-									{ INF,   1, INF,   0,   1,   1 },
-									{ INF,   1, INF,   1,   0,   1 },
-									{ INF, INF, INF,   1,   1,   0 } };
+int matrix[VERTICES][VERTICES]  = { {   0, INF,   1, INF, INF, INF },
+                                    { INF,   0, INF,   1,   1, INF },
+                                    {   1, INF,   0, INF, INF, INF },
+                                    { INF,   1, INF,   0,   1,   1 },
+                                    { INF,   1, INF,   1,   0,   1 },
+                                    { INF, INF, INF,   1,   1,   0 } };
 
-// Método recursivo que encontra os componentes conexos a partir de uma matriz de adjacências
-void calculaComponentesConexos(int atual){
-	for (int i = 0; i < VERTICES; i++){
-		if( visitados[i] == false && matriz[atual][i] == 1 ){
-			visitados[i] = true;
-			componentes++;
-			printf("(%d)-", i);
-			calculaComponentesConexos(i);
-		}
-	}
+// Recursive method that finds the connected components from an adjacency matrix
+void calculateConnectedComponents(int current){
+    for (int i = 0; i < VERTICES; i++){
+        if( visited[i] == false && matrix[current][i] == 1 ){
+            visited[i] = true;
+            components++;
+            printf("(%d)-", i);
+            calculateConnectedComponents(i);
+        }
+    }
 }
 
 int main(){
-	for (int i = 0; i < VERTICES; i++)
-		visitados[i] = false;
+    for (int i = 0; i < VERTICES; i++)
+        visited[i] = false;
 
-	for (int i = 0; i < VERTICES; i++)
-		if( visitados[i] == false ){
-			componentes = 0;
-			calculaComponentesConexos(i);
-			printf("\nNumero de componentes conexos iniciando pelo vertice %d: %d\n\n", i, componentes);
-		}
-	
-	return 0;
+    for (int i = 0; i < VERTICES; i++)
+        if( visited[i] == false ){
+            components = 0;
+            calculateConnectedComponents(i);
+            printf("\nNumber of connected components starting from vertex %d: %d\n\n", i, components);
+        }
+    
+    return 0;
 }
