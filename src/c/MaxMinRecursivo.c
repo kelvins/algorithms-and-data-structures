87f6a6c1c8f8d95b2e4a82febc03aea5c837d614
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:13:23 +0330

    Update MaxMinRecursivo.c

--- src/c/MaxMinRecursivo.c
+++ src/c/MaxMinRecursivo.c
@@ -1,26 +1,26 @@
 /*
-*	Exemplo de algoritmo recursivo.
-*	Objetivo: encontrar o valor máximo e mínimo em um vetor, utilizando recursividade
+*	Example of a recursive algorithm.
+*	Objective: find the maximum and minimum values in an array using recursion.
 *	Kelvin Salton do Prado - 2015
 */
 
 #include <stdio.h>
 
-#define TAM 10
+#define SIZE 10
 
-void MaxMin(int vetor[], int min, int max, int indice){
-	if( vetor[indice] < min )
-		min = vetor[indice];
-	if( vetor[indice] > max )
-		max = vetor[indice];
-	if( indice < TAM-1 )
-		MaxMin(vetor, min, max, indice+1);
+void MaxMin(int array[], int min, int max, int index){
+	if( array[index] < min )
+		min = array[index];
+	if( array[index] > max )
+		max = array[index];
+	if( index < SIZE - 1 )
+		MaxMin(array, min, max, index + 1);
 	else
 		printf("Min: %d \nMax: %d\n\n", min, max);
 }
 
 int main(){
-	int vetor[TAM] = {9,2,1,8,6,3,4,5,0,7};
-	MaxMin(vetor, vetor[0], vetor[0], 0);
+	int array[SIZE] = {9,2,1,8,6,3,4,5,0,7};
+	MaxMin(array, array[0], array[0], 0);
 	return 0;
 }
