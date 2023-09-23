03b748ca63b884c7dd4749aaec9c3b5e625d9412
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:13:00 +0330

    Update ListaSequencialOrdenada.c

--- src/c/ListaSequencialOrdenada.c
+++ src/c/ListaSequencialOrdenada.c
@@ -1,105 +1,104 @@
 /*
-*	Exemplo de implementação de Lista Sequencial Ordenada em C - Utilizando sentinela
+*	Example of Ordered Sequential List Implementation in C - Using a sentinel
 *	Kelvin S. do Prado - 2015
 */
 
 #include <stdio.h>
 
 #define MAX 10
-#define ERRO -1
+#define ERROR -1
 
-typedef int TIPOCHAVE; // Define um nome TIPOCHAVE para um tipo inteiro
+typedef int KEY_TYPE; // Defines a type alias KEY_TYPE for an integer
 
-typedef struct{
-	TIPOCHAVE chave;
-}REGISTRO;
+typedef struct {
+    KEY_TYPE key;
+} RECORD;
 
-typedef struct{
-	REGISTRO A[MAX+1]; // O +1 é a posição que será utilizada para a 'sentinela'
-	int nroElementos;
-}LISTA;
+typedef struct {
+    RECORD A[MAX + 1]; // The +1 is for the position used by the 'sentinel'
+    int nroElements;
+} LIST;
 
-void inicializar(LISTA* L){
-	L->nroElementos = 0; // Acessa a lista pelo endereço de memória
-	int i = 0;
-	for (i; i < MAX-2; ++i){ // Preenche a lista até -2 para deixar espaço para inserir mais depois
-		L->A[i].chave = i*2;
-	}
-	L->nroElementos = MAX-2;
-	// (*L).nroElementos = 0; // Neste caso iria acessar a lista em si, e não o ponteiro
+void initialize(LIST* L) {
+    L->nroElements = 0; // Accesses the list through the memory address
+    int i = 0;
+    for (i; i < MAX - 2; ++i) { // Fills the list up to -2 to leave space for insertion later
+        L->A[i].key = i * 2;
+    }
+    L->nroElements = MAX - 2;
+    // (*L).nroElements = 0; // In this case, it would access the list itself, not the pointer
 }
 
-/* A função do sentinela é adicionar a chave ao final da lista, ou seja, 
- * sempre irá encontrar a chave, mesmo que seja na útlima posição da lista.
- * Caso seja o último elemento, significa que não encontrou.
- * Deste modo elimina o 'if' dentro do laço, poupando várias comparações
+/* The sentinel's function is to add the key to the end of the list, 
+ * so it will always find the key even if it's in the last position of the list.
+ * If it's the last element, it means it wasn't found.
+ * This way, it eliminates the 'if' statement inside the loop, saving multiple comparisons.
  */ 
-int buscaSentinela(TIPOCHAVE ch, LISTA* L){ // Poderia usar aqui busca binária, o que seria mais apropriado.
-	int i = 0;
-	L->A[L->nroElementos].chave = ch; // Atribui a 'chave'/valor buscado a ultima posição do array A
-	while(L->A[i].chave != ch) // Percorre todo o array A buscando se a 'chave'/valor pesquisado se encontra no array (senão será o sentinela)
-		i++;
-	if(i == L->nroElementos) // Se o valor chegou até o final, significa que não encontrou o valor, retorna ERRO (-1)
-		return ERRO;
-	return i; // Caso contrário retorna a posição do valor/'chave' no array
+int sentinelSearch(KEY_TYPE ch, LIST* L) { // Binary search could be used here, which would be more appropriate.
+    int i = 0;
+    L->A[L->nroElements].key = ch; // Assigns the 'key'/value being searched to the last position of array A
+    while (L->A[i].key != ch) // Scans the entire array A to find if the 'key'/value being searched is in the array (if not, it will be the sentinel)
+        i++;
+    if (i == L->nroElements) // If the value reached the end, it means the value was not found, so it returns ERROR (-1)
+        return ERROR;
+    return i; // Otherwise, it returns the position of the value/'key' in the array
 }
 
-bool inserirOrdenado(REGISTRO reg, LISTA* L){
-	int i = 0;
-	if(L->nroElementos >= MAX)
-		return false;
-	L->A[L->nroElementos].chave = reg.chave;
-	while(L->A[i].chave < reg.chave)
-		i++;
-	int p = L->nroElementos-1;
-	while(p >= i){
-		L->A[p+1] = L->A[p];
-		p--;
-	}
-	L->A[i] = reg;
-	L->nroElementos++;
-	return true;
+bool insertOrdered(RECORD reg, LIST* L) {
+    int i = 0;
+    if (L->nroElements >= MAX)
+        return false;
+    L->A[L->nroElements].key = reg.key;
+    while (L->A[i].key < reg.key)
+        i++;
+    int p = L->nroElements - 1;
+    while (p >= i) {
+        L->A[p + 1] = L->A[p];
+        p--;
+    }
+    L->A[i] = reg;
+    L->nroElements++;
+    return true;
 }
 
-bool deletaValor(REGISTRO reg, LISTA* L){
-	int posicao = buscaSentinela(reg.chave, L);
-	if( posicao >= 0 ){
-		for( posicao; posicao < L->nroElementos; posicao++ ){
-			L->A[posicao] = L->A[posicao+1];
-		}
-		L->nroElementos--;
-		return true;
-	}else{
-		return false;
-	}
+bool deleteValue(RECORD reg, LIST* L) {
+    int position = sentinelSearch(reg.key, L);
+    if (position >= 0) {
+        for (position; position < L->nroElements; position++) {
+            L->A[position] = L->A[position + 1];
+        }
+        L->nroElements--;
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void mostraLista(LISTA* L){
-	int i = 0;
-	for (i; i < L->nroElementos; ++i){ // Percorre e mostra todos os valores do array
-		printf("%d, ", L->A[i].chave);
-	}
-	printf("\n\n");
+void showList(LIST* L) {
+    int i = 0;
+    for (i; i < L->nroElements; ++i) { // Iterates and displays all values in the array
+        printf("%d, ", L->A[i].key);
+    }
+    printf("\n\n");
 }
 
-int main(){
-	
-	LISTA LISTA;
-	inicializar(&LISTA);
+int main() {
+    LIST LIST;
+    initialize(&LIST);
 
-	printf("Valor 10 encontrado na posição: %d\n\n", buscaSentinela(10, &LISTA) );
-	mostraLista(&LISTA);
+    printf("Value 10 found at position: %d\n\n", sentinelSearch(10, &LIST));
+    showList(&LIST);
 
-	REGISTRO reg;
-	reg.chave = 7;
-	printf("Insere o valor: %d\n", reg.chave);
-	inserirOrdenado(reg, &LISTA);
-	mostraLista(&LISTA);
+    RECORD reg;
+    reg.key = 7;
+    printf("Inserting value: %d\n", reg.key);
+    insertOrdered(reg, &LIST);
+    showList(&LIST);
 
-	reg.chave = 12;
-	printf("Deleta o valor: %d\n", reg.chave);
-	deletaValor(reg, &LISTA);
-	mostraLista(&LISTA);
-	
-	return 0;
+    reg.key = 12;
+    printf("Deleting value: %d\n", reg.key);
+    deleteValue(reg, &LIST);
+    showList(&LIST);
+    
+    return 0;
 }
