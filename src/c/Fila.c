53e864f52592f70c752a10022a54c83d8d53087d
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:08:18 +0330

    Update Fila.c

--- src/c/Fila.c
+++ src/c/Fila.c
@@ -1,81 +1,82 @@
 /*
-*	Exemplo de implementação de Fila em C
+*	Example of Queue Implementation in C
 *	Kelvin S. do Prado
 */
 
 #include <stdio.h>
+#include <stdbool.h> // Added for boolean support
 
 #define MAX 10
-#define ERRO -1
+#define ERROR -1
 
-typedef int TIPOCHAVE;
+typedef int KEY;
 
-typedef struct{
-	TIPOCHAVE chave;
-}REGISTRO;
+typedef struct {
+    KEY key;
+} RECORD;
 
-typedef struct{
-	REGISTRO A[MAX+1];
-	int nroRegistros;
-}FILA;
+typedef struct {
+    RECORD A[MAX + 1];
+    int numberOfRecords;
+} QUEUE;
 
-void inicializa(FILA* F){
-	int i = 0;
-	for (i; i < MAX-2; i++){
-		F->A[i].chave = i*2;
-	}
-	F->nroRegistros = i;
+void initialize(QUEUE* Q) {
+    int i = 0;
+    for (i; i < MAX - 2; i++) {
+        Q->A[i].key = i * 2;
+    }
+    Q->numberOfRecords = i;
 }
 
-void mostraFila(FILA* F){
-	int i = 0;
-	printf("FILA:\n");
-	for (i; i < F->nroRegistros; i++){
-		printf("[ %d ] ", F->A[i].chave);
-	}
-	printf("\n\n");
+void showQueue(QUEUE* Q) {
+    int i = 0;
+    printf("QUEUE:\n");
+    for (i; i < Q->numberOfRecords; i++) {
+        printf("[ %d ] ", Q->A[i].key);
+    }
+    printf("\n\n");
 }
 
-bool insereFila(TIPOCHAVE ch, FILA* F){
-	if( F->nroRegistros >= MAX )
-		return false;
-	F->A[F->nroRegistros].chave = ch;
-	F->nroRegistros++;
-	return true;
+bool enqueue(KEY k, QUEUE* Q) {
+    if (Q->numberOfRecords >= MAX)
+        return false;
+    Q->A[Q->numberOfRecords].key = k;
+    Q->numberOfRecords++;
+    return true;
 }
 
-bool removeFila(FILA* F){
-	if( F->nroRegistros <= 0 )
-		return false;
-	int i = 1;
-	for (i; i < F->nroRegistros; i++){
-		F->A[i-1].chave = F->A[i].chave;
-	}
-	F->nroRegistros--;
-	return true;
+bool dequeue(QUEUE* Q) {
+    if (Q->numberOfRecords <= 0)
+        return false;
+    int i = 1;
+    for (i; i < Q->numberOfRecords; i++) {
+        Q->A[i - 1].key = Q->A[i].key;
+    }
+    Q->numberOfRecords--;
+    return true;
 }
 
-int buscaFila(TIPOCHAVE ch, FILA* F){
-	F->A[F->nroRegistros].chave = ch; // Coloca a ch na ultima posição para fazer busca Sentinela
-	int i = 0;
-	while( F->A[i].chave != ch )
-		i++;
-	if( i >= F->nroRegistros )
-		return ERRO;
-	return i;
+int searchQueue(KEY k, QUEUE* Q) {
+    Q->A[Q->numberOfRecords].key = k; // Place k in the last position for sentinel search
+    int i = 0;
+    while (Q->A[i].key != k)
+        i++;
+    if (i >= Q->numberOfRecords)
+        return ERROR;
+    return i;
 }
 
-int main(){
-	FILA fi;
-	inicializa(&fi);
-	mostraFila(&fi);
+int main() {
+    QUEUE queue;
+    initialize(&queue);
+    showQueue(&queue);
 
-	insereFila(15, &fi);
-	mostraFila(&fi);
+    enqueue(15, &queue);
+    showQueue(&queue);
 
-	removeFila(&fi);
-	mostraFila(&fi);
+    dequeue(&queue);
+    showQueue(&queue);
 
-	printf("A chave buscada se encontra na posicao %d da fila\n\n", buscaFila(8, &fi) );
-	return 0;
+    printf("The searched key is located at position %d in the queue\n\n", searchQueue(8, &queue));
+    return 0;
 }
