6c9f1e63541497985def8308ccb852fd97cb800c
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:02:23 +0330

    Update BuscaSequencial.c

--- src/c/BuscaSequencial.c
+++ src/c/BuscaSequencial.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-int buscaSequencial(int vetor[], int size, int buscado) {
-    for (int i = 0; i < size; i++)
-    {
-        if (vetor[i] == buscado)
-        {
-            return i;
+int sequentialSearch(int array[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == target) {
+            return i; // Return the index where the target value was found
         }
     }
-    return -1;    
+    return -1; // Return -1 if the target value was not found in the array
 }
 
 int main(){
 
     int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int n = sizeof(a) / sizeof(a[0]);
-    printf("Valor %d no índice %d\n", 3, buscaSequencial(a, n, 3));
-    printf("Valor %d no índice %d\n", 9, buscaSequencial(a, n, 9));
+    printf("Value %d found at index %d\n", 3, sequentialSearch(a, n, 3));
+    printf("Value %d found at index %d\n", 9, sequentialSearch(a, n, 9));
 }
-
