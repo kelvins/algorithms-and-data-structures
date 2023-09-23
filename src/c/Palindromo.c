8de4c50a0695b1e52c767fabaf50a415d1ff2783
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-23 06:14:46 +0330

    Update Palindromo.c

--- src/c/Palindromo.c
+++ src/c/Palindromo.c
@@ -3,12 +3,13 @@
 
 #define MAX_SIZE_WORD 100
 
+// Function to calculate the reverse of a string
 void calc_reverse(char *input, char *output) {
   size_t len_input = strlen(input);
 
   char *last_char = input + len_input - 1;
 
-  for (int i=0; i < len_input; i++) {
+  for (int i = 0; i < len_input; i++) {
     output[i] = *(last_char - i);
   }
   output[len_input] = '\0';
@@ -18,20 +19,22 @@ int main() {
   char input[MAX_SIZE_WORD];
   char reverse[MAX_SIZE_WORD];
 
-  printf("Digite uma palavra: ");
+  printf("Enter a word: ");
 
   fgets(input, MAX_SIZE_WORD, stdin);
-  //remove New Line from the end
+  // Remove New Line from the end
   input[strlen(input) - 1] = '\0';
 
+  // Calculate the reverse of the input word
   calc_reverse(input, reverse);
 
-  printf("Sua palavra invertida: %s\n", reverse);
+  printf("Your reversed word: %s\n", reverse);
 
+  // Check if the input word is a palindrome
   if (!strcmp(input, reverse))
-    puts("Sua palavra eh um palindromo!");
+    puts("Your word is a palindrome!");
   else
-    puts("Sua palavra NAO eh um palindromo!");
+    puts("Your word is NOT a palindrome!");
 
   return 0;
 }
