d37c38d488397dcc8208c2fccdcda4fd372e85f4
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-09-25 02:01:08 +0530

    Fixing Lint error

--- src/python/arvore_binaria_de_busca.py
+++ src/python/arvore_binaria_de_busca.py
@@ -4,180 +4,178 @@ Kelvin Salton do Prado
 2015
 """
 
-class Arvore:
-    def __init__(self, chave):
-        self.chave = chave
-        self.esquerda = None
-        self.direita = None
+class Tree:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
 
 # Search Methods
-def busca_recursiva(no, chave):
-    if no is None:
-        print(f"{chave} nao foi encontrado na arvore")
+def recursive_search(node, key):
+    if node is None:
+        print(f"{key} was not found in the tree")
         return
-    if no.chave == chave:
-        print(f"{chave} foi encontrado na arvore")
-        return no
-    if chave > no.chave:
-        busca_recursiva(no.direita, chave)
+    if node.key == key:
+        print(f"{key} was found in the tree")
+        return node
+    if key > node.key:
+        recursive_search(node.right, key)
     else:
-        busca_recursiva(no.esquerda, chave)
+        recursive_search(node.left, key)
 
 
-def busca_linear(no, chave):
-    while no is not None:
-        if no.chave == chave:
-            return no
-        elif chave > no.chave:
-            no = no.direita
+def linear_search(node, key):
+    while node is not None:
+        if node.key == key:
+            return node
+        elif key > node.key:
+            node = node.right
         else:
-            no = no.esquerda
+            node = node.left
     return None
 
 
 # Insertion Method
-def insere(no, chave):
-    if no is None:
-        no = Arvore(chave)
+def insert(node, key):
+    if node is None:
+        node = Tree(key)
     else:
-        if chave < no.chave:
-            no.esquerda = insere(no.esquerda, chave)
+        if key < node.key:
+            node.left = insert(node.left, key)
         else:
-            no.direita = insere(no.direita, chave)
-    return no
+            node.right = insert(node.right, key)
+    return node
 
 
 # Printing Methods
-IMPRIME_ARVORE = ""
+PRINT_TREE = ""
 
 
-def pre_ordem(no):
-    global IMPRIME_ARVORE
-    if no is None:
+def pre_order(node):
+    global PRINT_TREE
+    if node is None:
         return
-    IMPRIME_ARVORE += str(no.chave) + ", "
-    pre_ordem(no.esquerda)
-    pre_ordem(no.direita)
+    PRINT_TREE += str(node.key) + ", "
+    pre_order(node.left)
+    pre_order(node.right)
 
 
-def em_ordem(no):
-    global IMPRIME_ARVORE
-    if no is None:
+def in_order(node):
+    global PRINT_TREE
+    if node is None:
         return
-    em_ordem(no.esquerda)
-    IMPRIME_ARVORE += str(no.chave) + ", "
-    em_ordem(no.direita)
+    in_order(node.left)
+    PRINT_TREE += str(node.key) + ", "
+    in_order(node.right)
 
 
-def pos_ordem(no):
-    global IMPRIME_ARVORE
-    if no is None:
+def post_order(node):
+    global PRINT_TREE
+    if node is None:
         return
-    pos_ordem(no.esquerda)
-    pos_ordem(no.direita)
-    IMPRIME_ARVORE += str(no.chave) + ", "
+    post_order(node.left)
+    post_order(node.right)
+    PRINT_TREE += str(node.key) + ", "
 
 
 # Find the Height of the Tree
-def maximo(a, b):
+def maximum(a, b):
     if a > b:
         return a
     return b
 
 
-def altura(no):
-    if no is None:
+def height(node):
+    if node is None:
         return 0
-    return 1 + maximo(altura(no.esquerda), altura(no.direita))
+    return 1 + maximum(height(node.left), height(node.right))
 
 
 # Exclusion Methods
-def busca_no_pai(no, ch):
-    no_pai = no
-    while no is not None:
-        if no.chave == ch:
-            return no_pai
-        no_pai = no
-        if no.chave < ch:
-            no = no.direita
+def find_parent(node, key):
+    parent = node
+    while node is not None:
+        if node.key == key:
+            return parent
+        parent = node
+        if node.key < key:
+            node = node.right
         else:
-            no = no.esquerda
-    return no_pai
+            node = node.left
+    return parent
 
 
-def maiorAesquerda(no):
-    no = no.esquerda
-    while no.direita is not None:
-        no = no.direita
-    return no
+def leftmost_larger(node):
+    node = node.left
+    while node.right is not None:
+        node = node.right
+    return node
 
 
-def exclui(no, ch):
-    atual = busca_linear(no, ch)
-    if atual is None:
+def delete(node, key):
+    current = linear_search(node, key)
+    if current is None:
         return False
-    noPai = busca_no_pai(no, ch)
-    if atual.esquerda is None or atual.direita is None:
-        if atual.esquerda is None:
-            substituto = atual.direita
+    parent = find_parent(node, key)
+    if current.left is None or current.right is None:
+        if current.left is None:
+            substitute = current.right
         else:
-            substituto = atual.esquerda
-        if noPai is None:
-            no = substituto
-        elif ch > noPai.chave:
-            noPai.direita = substituto
+            substitute = current.left
+        if parent is None:
+            node = substitute
+        elif key > parent.key:
+            parent.right = substitute
         else:
-            noPai.esquerda = substituto
-        # Here is the free (current)
+            parent.left = substitute
     else:
-        substituto = maiorAesquerda(atual)
-        atual.chave = substituto.chave
-        if substituto.esquerda is not None:
-            atual.esquerda = substituto.esquerda
+        substitute = leftmost_larger(current)
+        current.key = substitute.key
+        if substitute.left is not None:
+            current.left = substitute.left
         else:
-            atual.esquerda = None
-        # FREE(replacement)
+            current.left = None
     return True
 
 
 if __name__ == "__main__":
-    arvore = Arvore(3)  # Create tree (root)
+    tree = Tree(3)  # Create tree (root)
     # Insert multiple values into the tree.
-    arvore = insere(arvore, 2)
-    arvore = insere(arvore, 1)
-    arvore = insere(arvore, 4)
-    arvore = insere(arvore, 6)
-    arvore = insere(arvore, 8)
-    arvore = insere(arvore, 5)
-    arvore = insere(arvore, 7)
-    arvore = insere(arvore, 0)
+    tree = insert(tree, 2)
+    tree = insert(tree, 1)
+    tree = insert(tree, 4)
+    tree = insert(tree, 6)
+    tree = insert(tree, 8)
+    tree = insert(tree, 5)
+    tree = insert(tree, 7)
+    tree = insert(tree, 0)
 
-    busca_recursiva(arvore, 6)  # Search that prints within the function.
+    recursive_search(tree, 6)  # Search that prints within the function.
 
-    if busca_linear(arvore, 6) is not None:  # Return the NODE or None if not found.
-        print("Valor encontrado")
+    if linear_search(tree, 6) is not None:  # Return the NODE or None if not found.
+        print("Value found")
     else:
-        print("Valor nao encontrado")
+        print("Value not found")
 
-    print(f"Altura: {altura(arvore)}")
+    print(f"Height: {height(tree)}")
 
     # Delete multiple values.
-    exclui(arvore, 7)
-    exclui(arvore, 5)
-    exclui(arvore, 8)
-    exclui(arvore, 3)
+    delete(tree, 7)
+    delete(tree, 5)
+    delete(tree, 8)
+    delete(tree, 3)
 
     # Call the print methods.
-    IMPRIME = ""
-    pre_ordem(arvore)
-    print(f"PreOrdem: {IMPRIME}")
-    IMPRIME = ""
-    em_ordem(arvore)
-    print(f"EmOrdem: {IMPRIME}")
-    IMPRIME = ""
-    pos_ordem(arvore)
-    print(f"PosOrdem: {IMPRIME}")
+    PRINT_TREE = ""
+    pre_order(tree)
+    print(f"PreOrder: {PRINT_TREE}")
+    PRINT_TREE = ""
+    in_order(tree)
+    print(f"InOrder: {PRINT_TREE}")
+    PRINT_TREE = ""
+    post_order(tree)
+    print(f"PostOrder: {PRINT_TREE}")
 
     # Show the height of the tree after removing the items.
-    print(f"Altura: {altura(arvore)}")
+    print(f"Height: {height(tree)}")
--- src/python/busca_em_grafo.py
+++ src/python/busca_em_grafo.py
@@ -3,29 +3,29 @@
 from collections import deque
 
 
-class Grafo:
+class Graph:
     """Define a graph using an adjacency matrix.
 
     Args:
-    edges (list): a list of lists where the index represents the vertex, 
+    edges (list): a list of lists where the index represents the vertex,
     and each element in the list is the neighbor.
     """
 
-    def __init__(self, arestas: list):
-        self.adj = [[] for _ in range(len(arestas))]
-        self.adiciona_arestas(arestas)
+    def __init__(self, edges: list):
+        self.adj = [[] for _ in range(len(edges))]
+        self.add_edges(edges)
 
-    def adiciona_arestas(self, arestas: list) -> None:
+    def add_edges(self, edges: list) -> None:
         """Adds all the edges to the graph.
 
         Args:
-            Edges (list): the list containing all the edge definitions of the graph.
+            edges (list): the list containing all the edge definitions of the graph.
         """
-        for i in range(len(arestas)):
-            for j in range(len(arestas[i])):
-                self.__adiciona_aresta(i, arestas[i][j])
+        for i in range(len(edges)):
+            for j in range(len(edges[i])):
+                self._add_edge(i, edges[i][j])
 
-    def __adiciona_aresta(self, u: int, v: int) -> None:
+    def _add_edge(self, u: int, v: int) -> None:
         """Adds the edge to the adjacency matrix.
 
         Args:
@@ -47,20 +47,20 @@ class Grafo:
         Returns:
             list: a list containing the order of visited vertices.
         """
-        fila = deque()
-        fila.append(start)
-        visitados = []
-        visitados.append(start)
+        queue = deque()
+        queue.append(start)
+        visited = []
+        visited.append(start)
 
-        while fila:
-            u = fila.popleft()
+        while queue:
+            u = queue.popleft()
 
             for v in self.adj[u]:
-                if v not in visitados:
-                    fila.append(v)
-                    visitados.append(v)
+                if v not in visited:
+                    queue.append(v)
+                    visited.append(v)
 
-        return visitados
+        return visited
 
     def dfs(self, start: int) -> list:
         """Performs depth-first search starting from the vertex "start."
@@ -71,24 +71,25 @@ class Grafo:
         Returns:
             list: a list containing the order of visited vertices.
         """
-        visitados = []
-        visitados.append(start)
-        pilha = [start]
+        visited = []
+        visited.append(start)
+        stack = [start]
 
-        while pilha:
-            u = pilha.pop()
+        while stack:
+            u = stack.pop()
 
-            if u not in visitados:
-                visitados.append(u)
+            if u not in visited:
+                visited.append(u)
 
-            for v in self.adj[u][::-1]:
-                if v not in visitados:
-                    pilha.append(v)
-
-        return visitados
+            for v in reversed(self.adj[u]):
+                if v not in visited:
+                    stack.append(v)
 
 
-arestas = [
+        return visited
+
+
+edges = [
     [1, 2, 5],  # Neighbors of vertex 0.
     [0, 2],  # Neighbors of vertex 1.
     [0, 1, 3, 4],  # Neighbors of vertex 2.
@@ -97,7 +98,7 @@ arestas = [
     [0, 3]  # Neighbors of vertex 5.
 ]
 
-grafo = Grafo(arestas)
-print(grafo.adj)
-print(grafo.bfs(0))
-print(grafo.dfs(0))
+graph = Graph(edges)
+print(graph.adj)
+print(graph.bfs(0))
+print(graph.dfs(0))
