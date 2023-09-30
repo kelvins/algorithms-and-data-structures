16c6ce164cfee69dac62ad406971e98c7095dd6d
leandronsp <leandronsp@gmail.com>
2023-09-30 10:33:16 -0300

    applying go fmt

--- src/go/deque.go
+++ src/go/deque.go
@@ -1,64 +1,66 @@
+package main
+
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 type Deque struct {
-  Store []int
+	Store []int
 }
 
 // Time complexity: O(1)
 func (deque *Deque) RPush(element int) {
-  deque.Store = append(deque.Store, element)
+	deque.Store = append(deque.Store, element)
 }
 
 // Time complexity: O(n)
 func (deque *Deque) LPush(element int) {
-  deque.Store = append([]int{element}, deque.Store...)
+	deque.Store = append([]int{element}, deque.Store...)
 }
 
 // Time complexity: O(1)
 func (deque *Deque) RPop() *int {
-  if len(deque.Store) == 0 {
-    return nil
-  }
+	if len(deque.Store) == 0 {
+		return nil
+	}
 
-  element := deque.Store[len(deque.Store) - 1]
-  deque.Store = deque.Store[:len(deque.Store) - 1]
+	element := deque.Store[len(deque.Store)-1]
+	deque.Store = deque.Store[:len(deque.Store)-1]
 
-  return &element
+	return &element
 }
 
 // Time complexity: O(n)
 func (deque *Deque) LPop() *int {
-  if len(deque.Store) == 0 {
-    return nil
-  }
+	if len(deque.Store) == 0 {
+		return nil
+	}
 
-  element := deque.Store[0]
-  deque.Store = deque.Store[1:]
+	element := deque.Store[0]
+	deque.Store = deque.Store[1:]
 
-  return &element
+	return &element
 }
 
 func TestDeque(t *testing.T) {
-  deque := Deque{}
+	deque := Deque{}
 
-  deque.RPush(1)
-  deque.RPush(2)
-  deque.RPush(3)
+	deque.RPush(1)
+	deque.RPush(2)
+	deque.RPush(3)
 
-  assert.Equal(t, 3, *deque.RPop())
-  assert.Equal(t, 2, *deque.RPop())
-  assert.Equal(t, 1, *deque.RPop())
-  assert.Nil(t, deque.RPop())
+	assert.Equal(t, 3, *deque.RPop())
+	assert.Equal(t, 2, *deque.RPop())
+	assert.Equal(t, 1, *deque.RPop())
+	assert.Nil(t, deque.RPop())
 
-  deque.LPush(1)
-  deque.LPush(2)
-  deque.LPush(3)
+	deque.LPush(1)
+	deque.LPush(2)
+	deque.LPush(3)
 
-  assert.Equal(t, 1, *deque.RPop())
-  assert.Equal(t, 2, *deque.RPop())
-  assert.Equal(t, 3, *deque.RPop())
-  assert.Nil(t, deque.RPop())
+	assert.Equal(t, 1, *deque.RPop())
+	assert.Equal(t, 2, *deque.RPop())
+	assert.Equal(t, 3, *deque.RPop())
+	assert.Nil(t, deque.RPop())
 }
