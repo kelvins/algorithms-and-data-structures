65d4385f21d1063680fd8048e403de4ab5de96d3
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-07 18:08:24 -0300

    Update sequentialSearch.swift

--- src/swift/sequentialSearch.swift
+++ src/swift/sequentialSearch.swift
@@ -10,3 +10,18 @@ func sequentialSearch(array: [Int], searchedElement: Int) -> Int {
     }
     return -1  // If element isn't found
 }
+
+func main() {
+    let numbers = [4, 7, 2, 9, 1, 5, 8]
+    let elementToSearch = 5
+
+    let result = sequentialSearch(array: numbers, searchedElement: elementToSearch)
+
+    if result != -1 {
+        print("Element \(elementToSearch) found at index \(result)")
+    } else {
+        print("Element \(elementToSearch) not found in the array.")
+    }
+}
+
+main()
