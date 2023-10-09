765818f56493c6f688bc52af6f70bd87b457b523
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-09 09:14:06 -0300

    Update RecursiveLinearSearch.scala

--- src/scala/RecursiveLinearSearch.scala
+++ src/scala/RecursiveLinearSearch.scala
@@ -2,18 +2,14 @@ import scala.annotation.tailrec
 
 @tailrec
 def search(data: List[Int], value: Int, index: Int = 0): Int = {
-  if (index >= data.size) {
-    -1
-  } else if (data(index) == value) {
-    index
-  } else {
-    search(data, value, index + 1)
-  }
+  if (index >= data.size) -1
+  else if (data(index) == value) index
+  else search(data, value, index + 1)
 }
 
 object Main extends App {
-  val data: List[Int] = List(9, 2, 6, 1, 3, 5, 4, 7, 8)
-  val values: List[Int] = List(9, 3, 8, 246)
+  val data: List[Int]    = List(9, 2, 6, 1, 3, 5, 4, 7, 8)
+  val values: List[Int]  = List(9, 3, 8, 246)
   val results: List[Int] = values.map(v => search(data, v))
 
   values
