0f62e031c6c5a4a4d680396bb5fc9723b260a784
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-09 09:03:47 -0300

    Update and rename BuscaSequencialRecursiva.scala to RecursiveLinearSearch.scala

---
+++ src/scala/RecursiveLinearSearch.scala
@@ -0,0 +1,22 @@
+import scala.annotation.tailrec
+
+@tailrec
+def search(data: List[Int], value: Int, index: Int = 0): Int = {
+  if (index >= data.size) {
+    -1
+  } else if (data(index) == value) {
+    index
+  } else {
+    search(data, value, index + 1)
+  }
+}
+
+object Main extends App {
+  val data: List[Int] = List(9, 2, 6, 1, 3, 5, 4, 7, 8)
+  val values: List[Int] = List(9, 3, 8, 246)
+  val results: List[Int] = values.map(v => search(data, v))
+
+  values
+    .zip(results)
+    .foreach((value, result) => println(s"Value: $value -> Index: $result"))
+}
