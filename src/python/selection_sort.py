9260e21ba6ab321952c5831b77f21795bd422f32
Ananya <singh.ananya4458@gmail.com>
2023-10-11 12:14:57 +0530

    Updated the error

--- src/python/selection_sort.py
+++ src/python/selection_sort.py
@@ -1,32 +1,13 @@
-"""Implementation of a selection sort algorithm with recursion."""
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
-def selection_sort(arr, index):
-    """
-    Implementation of a selection sort algorithm with recursion.
-    Args:
-    arr (list): The list to be sorted.
-    index (int): The index of the element to be sorted in the list.
-    """
-    if index >= len(arr) - 1:
-        return
-
-    # min_index holds the position of the smallest value relative to the index
-    min_index = index
-
-    for i in range(index + 1, len(arr)):
-        if arr[i] < arr[min_index]:
-            min_index = i
-
-    temp = arr[index]
-    arr[index] = arr[min_index]
-    arr[min_index] = temp
-
-    selection_sort(arr, index + 1)
-
+# Example usage:
 unsorted_list = [82, 83, 92, 12, 23, 45, 64, 91, 73]
-
 print(unsorted_list)
-selection_sort(unsorted_list, 0)
+selection_sort(unsorted_list)
 print(unsorted_list)
-
-
