cd5fe02126109653db577b349d7d541b1161beac
Ananya <singh.ananya4458@gmail.com>
2023-10-11 14:38:46 +0530

    Updated

--- src/ruby/busca_binaria.rb
+++ src/ruby/busca_binaria.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def binary_search(value, array, left, right)
-  middle = ((left + right) / 2).floor
+  middle = ((left + right) / 2).truncate
 
   return -1 unless left <= right
 
