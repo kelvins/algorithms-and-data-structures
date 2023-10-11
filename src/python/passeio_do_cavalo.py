5609b0dd85acec0a8e99feb5810c2fcb14e0d226
Ananya <singh.ananya4458@gmail.com>
2023-10-11 11:17:24 +0530

    Updated passeio_do_cavalo.py

--- src/python/passeio_do_cavalo.py
+++ src/python/passeio_do_cavalo.py
@@ -1,70 +1,61 @@
-"""Implementação do algoritmo passeio do cavalo"""
+"""Implementation of the Knight's Tour algorithm"""
 
-
-def aceitavel(x, y):
+def is_acceptable(x, y):
     """
-    Aceitavel se estiver dentro do tabuleiro e a casa ainda nao tiver sido
-    visitada
+    Acceptable if it's within the board and the cell hasn't been visited yet.
 
-    Retorna True ou False
+    Returns True or False.
     """
-    if x >= 0 and x <= num - 1 and y >= 0 and y <= num - 1 and tabuleiro[x][y] == 0:
+    if 0 <= x < num and 0 <= y < num and board[x][y] == 0:
         return True
     else:
         return False
 
-
-def tenta_mover(i, x, y):
+def try_move(i, x, y):
     """
-    Tenta o i-esimo movimento em (x,y), 1 <= i <= n^2
+    Attempt the i-th move at (x, y), 1 <= i <= n^2.
     """
-    done = i > numSqr  # True ou False
+    done = i > num_square
     k = 0
     while not done and k < 8:
-        u = x + dx[k]  # Coordenadas dos 8 movimentos possiveis do cavalo
-        v = y + dy[k]  # Coordenadas dos 8 movimentos possiveis do cavalo
-        if aceitavel(u, v):
-            tabuleiro[u][v] = i
-            done = tenta_mover(i + 1, u, v)  # Tenta outro movimento
+        u = x + dx[k]  # Coordinates of the 8 possible knight moves
+        v = y + dy[k]  # Coordinates of the 8 possible knight moves
+        if is_acceptable(u, v):
+            board[u][v] = i
+            done = try_move(i + 1, u, v)
             if not done:
-                tabuleiro[u][v] = 0  # Sem sucesso, descarta movimento
-        k += 1  # Passa ao proximo movimento possivel
+                board[u][v] = 0  # Unsuccessful move, revert it
+        k += 1
     return done
 
-
-def mostra_movimento(x, y):
-    tabuleiro[x][y] = 1
-    done = tenta_mover(2, x, y)
-    string = ""
+def show_movement(x, y):
+    board[x][y] = 1
+    done = try_move(2, x, y)
+    result = ""
     if done:
         for x in range(0, num):
             for y in range(0, num):
-                if tabuleiro[x][y] < 10:
-                    string += "0" + str(tabuleiro[x][y]) + " "
+                if board[x][y] < 10:
+                    result += "0" + str(board[x][y]) + " "
                 else:
-                    string += str(tabuleiro[x][y]) + " "
-            string += "\n"
-        print(string)
+                    result += str(board[x][y]) + " "
+            result += "\n"
+        print(result)
     else:
-        print("Nao ha passeio possivel\n")
-
+        print("No possible tour available\n")
 
 dx = [2, 1, -1, -2, -2, -1, 1, 2]
 dy = [1, 2, 2, 1, -1, -2, -2, -1]
 
-print("Digite o num de posicoes do tabuleiro: (ex.: 6) <= 10")
-num = int(input())  # Numero de posicoes do tabuleiro
-print("Digite a posicao x onde o cavalo deve iniciar: (ex.: 1) >= 0")
-x = int(input())  # Numero de posicoes do tabuleiro
-print("Digite a posicao y onde o cavalo deve iniciar: (ex.: 2) >= 0")
-y = int(input())  # Numero de posicoes do tabuleiro
-numSqr = num * num  # Numero total de casas
+print("Enter the number of board positions: (e.g., 6) <= 10")
+num = int(input())  # Number of board positions
+print("Enter the x position where the knight should start: (e.g., 1) >= 0")
+x = int(input())  # X position where the knight should start
+print("Enter the y position where the knight should start: (e.g., 2) >= 0")
+y = int(input())  # Y position where the knight should start
+num_square = num * num  # Total number of cells on the board
 
 print()
 
-tabuleiro = [[], [], [], [], [], [], [], [], [], []]  # Tabuleiro maximo 20x20
-for x in range(0, num):
-    for y in range(0, num):
-        tabuleiro[x].append(0)
-# print tabuleiro
-mostra_movimento(x, y)
+board = [[0] * num for _ in range(num)]  # Maximum 20x20 board
+show_movement(x, y)
