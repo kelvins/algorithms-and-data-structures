8d5503b03b27740fe08f770e83d34a11a0afdd09
Ananya <singh.ananya4458@gmail.com>
2023-10-11 14:15:41 +0530

    Updated file busca_binaria.py

--- src/ruby/busca_binaria.rb
+++ src/ruby/busca_binaria.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
-def busca_binaria(valor, vetor, esquerda, direita)
-  meio = ((esquerda + direita) / 2).floor
+def binary_search(value, array, left, right)
+  middle = ((left + right) / 2).floor
 
-  return -1 unless esquerda <= direita
+  return -1 unless left <= right
 
-  if valor > vetor[meio]
-    busca_binaria(valor, vetor, meio + 1, direita)
-  elsif valor < vetor[meio]
-    busca_binaria(valor, vetor, esquerda, meio - 1)
+  if value > array[middle]
+    binary_search(value, array, middle + 1, right)
+  elsif value < array[middle]
+    binary_search(value, array, left, middle - 1)
   else
-    meio
+    middle
   end
 end
 
-vetor = [0, 1, 3, 5, 6, 7, 8, 9, 10, 11, 12]
-print busca_binaria(12, vetor, 0, vetor.length)
+array = [0, 1, 3, 5, 6, 7, 8, 9, 10, 11, 12]
+puts binary_search(12, array, 0, array.length)
+
