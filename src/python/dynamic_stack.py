75865f66cb8be4a371a16bfb3b623fad84b212fb
ribmarciojr <marciojunior2109@gmail.com>
2023-10-13 11:24:50 -0300

    refactor: add black formatter python default fmt

--- src/python/dynamic_stack.py
+++ src/python/dynamic_stack.py
@@ -1,5 +1,6 @@
 # Stack: Last In - First Out (LIFO)
 
+
 # Define node format: value and a pointier to next
 class Node:
     def __init__(self, value: int) -> None:
@@ -10,48 +11,49 @@ class Node:
 class DynamicStack:
     def __init__(self) -> None:
         self.__first = None
-    
+
     def isEmpty(self) -> bool:
         if not self.__first:
             return True
         return False
-    
-    def push(self,value: int):
+
+    def push(self, value: int):
         node = Node(value)
         node.next = self.__first
         self.__first = node
 
     def pop(self) -> int:
         if self.isEmpty():
-            raise Exception('Stack is empty!')
+            raise Exception("Stack is empty!")
         removedElement = self.__first.value
         self.__first = self.__first.next
         return removedElement
-        
 
     def show(self) -> None:
         node = self.__first
-        print('[', end=' ')
-        while(node):
-            print(node.value, end=' ')
+        print("[", end=" ")
+        while node:
+            print(node.value, end=" ")
             node = node.next
-        print(']')
+        print("]")
+
 
 def main() -> None:
     dynamic_stack = DynamicStack()
 
-    print(f'Push(1,2,4):')
+    print(f"Push(1,2,4):")
     dynamic_stack.push(1)
     dynamic_stack.push(2)
     dynamic_stack.push(4)
-    
+
     dynamic_stack.show()
 
-    print(f'Remove last: {dynamic_stack.pop()}')
-    print(f'Remove last: {dynamic_stack.pop()}')
-    print(f'Remove last: {dynamic_stack.pop()}')
-    print('Empty stack!')
+    print(f"Remove last: {dynamic_stack.pop()}")
+    print(f"Remove last: {dynamic_stack.pop()}")
+    print(f"Remove last: {dynamic_stack.pop()}")
+    print("Empty stack!")
     dynamic_stack.show()
+
 
 if __name__ == "__main__":
     main()
