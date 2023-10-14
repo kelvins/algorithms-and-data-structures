7e85484f705729e749c2baa3ee93ece59c8a430d
Gary Miller <gary@gmcubedesigns.com>
2023-10-14 03:48:08 -0400

    Create FibonacciIterative.js

---
+++ src/javascript/FibonacciIterative.js
@@ -0,0 +1,23 @@
+function fibonacciIterative(n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  let a = 0;
+  let b = 1;
+  let result;
+
+  for (let i = 2; i <= n; i++) {
+    result = a + b;
+    a = b;
+    b = result;
+  }
+
+  return result;
+}
+
+function main() {
+  console.log('Fibonacci (iterative) of 30:', fibonacciIterative(30));
+}
+
+main();
