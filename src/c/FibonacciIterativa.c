629c79e2bf1da74f880ef0952d878885fcb833e8
Gary Miller <gary@gmcubedesigns.com>
2023-10-14 00:43:05 -0400

    Update and rename Fibonacci.c to FibonacciIterativa.c

--- src/c/FibonacciIterativa.c
+++ src/c/FibonacciIterativa.c
@@ -12,20 +12,9 @@ int fibonacci_iterative(int number) {
     return last_number;
 }
 
-int fibonacci_recursive(int number) {
-    if (number == 0) {
-        return 0;
-    } else if (number == 1) {
-        return 1;
-    } else {
-        return fibonacci_recursive(number - 1) + fibonacci_recursive(number - 2);
-    }
-}
-
 int main(void) {
     int test_nbr = 12;
 
     printf("iterative: %d\n", fibonacci_iterative(test_nbr));
-    printf("recursive: %d\n", fibonacci_recursive(test_nbr));
     return 0;
 }
