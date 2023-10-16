1525a4b999112526c0df3bebbaacc160454954e6
DELTAREET <shatabdi2001chakraborty@gmail.com>
2023-10-16 23:38:04 +0530

    Here are the portuguese words translated into English

--- src/c/AlgoritmoDijkstra.c
+++ src/c/AlgoritmoDijkstra.c
@@ -1,12 +1,12 @@
 /*
-* Grafos - Algoritmo de Dijkstra em C
+* Graphs - Dijkstra Algorithm in C
 * Kelvin Salton do Prado - 2015
-* Complexidade: Teta(n^2)
+* Complexity: Theta(n^2)
 *
-* 1 para todos - Arestas de pesos não negativo - Algoritmo guloso
-* Encontra o caminho mais curto de um vértice (inicio) a outro (destino)
+* 1 for all - Edges with non-negative weights - Greedy algorithm
+* Finds the shortest path from one vertex (start) to another (destination)
 *
-* Grafo com 5 vértices e 6 arestas
+* Graph with 5 vertices and 6 edges
 *
 *              6
 *   (0)-----------------(1)
@@ -19,7 +19,7 @@
 *      \              /
 *       -----(4)-----
 *
-*  Matriz de Distância
+*  Distance Matrix
 *    0   1   2   3   4
 * 0  0   6  10   -   -
 * 1  6   0   -   2   -
@@ -27,44 +27,44 @@
 * 3  -   2   1   0   8
 * 4  -   -   3   8   0
 *
-*  Para valores infinitos será considerado o valor: 4294967295
-* O objetivo é sair do ponto inicial (0) e chegar ao destino (4) pelo caminho mais curto
-* Resposta: (0)->(1)->(3)->(2)->(4) = 12
+*  For infinite values, the value will be considered: 4294967295
+* The objective is to leave the starting point (0) and reach the destination (4) by the shortest route
+* Response: (0)->(1)->(3)->(2)->(4) = 12
 *
 */
 
 #include <stdio.h>
 #include <stdbool.h>
 
-#define nroVertices 5 // Define uma constante 5 que é a quantidade de vértices do grafo
+#define noVertices 5 // Defines a constant 5 which is the number of vertices in the graph
 
-// Algoritmo de Dijkstra recebe como parâmetro a matriz de distância e o número de vértices
-void Dijkstra(unsigned long int matriz[nroVertices][nroVertices], int n){
-    bool visitados[n]; // Variável que guarda true para os vértices visitados
+// Dijkstra's algorithm takes as parameters the distance matrix and the number of vertices
+void Dijkstra(unsigned long int matrix[noVertices][noVertices], int n){
+    
+bool visited[n]; // Variable that holds true for visited vertices
 
-    // O valor 'i' do for abaixo não é utilizado, pois o for serve apenas para percorrer todo o número de colunas da matriz
-    for(int i = 1; i < n; i++){ // Começa em 1 pois não precisa comparar o vértice com ele mesmo
+    // The value 'i' from the for below is not used, as the for is only used to traverse the entire number of columns in the matrix
+    for(int i = 1; i < n; i++){ // Starts at 1 because you don't need to compare the vertex with itself
 
-        int min = -1; // Variável que guarda a posição do menor valor, inicia em -1 pois é uma posição inválida
-        unsigned long int MinValor = 4294967295; // Variável que guarda o menor valor encontrado, inicia com 'infinito', assim, sempre na primeira passada o valor será menor que esta variável
-
-        // For que percorre todas as linhas na coluna [0]
+int min = -1; // Variable that stores the position of the smallest value, starts at -1 as it is an invalid position
+        unsigned long int MinValue = 4294967295; // Variable that stores the smallest value found, starts with 'infinity', so always on the first pass the value will be smaller than this variable
+        // For that loops through all rows in column [0]
         for(int j = 1; j < n; j++){
-            // Se o vertice ainda não foi visitado e o valor for menor que o 'MinValor'
-            if( !visitados[j] && matriz[j][0] < MinValor ){
-                   min = j; // Guarda a posição do menor
-                   MinValor = matriz[j][0]; // Guarda o menor valor
+            // If the vertex has not yet been visited and the value is less than the 'MinValor'
+            if( !visited[j] && matrix[j][0] < MinValue ){
+                   min = j; // Saves the position of the smallest
+                   MinValue = matrix[j][0]; // Save the smallest value
             }
           }
 
-          visitados[min] = true; // Marca o valor a posição do minimo como visitado
+          visited[min] = true; // Mark the value of the minimum position as visited
 
-        // For de 1 até n
+        // Goes from 1 to n
           for(int j = 1; j < n; j++){
-               // Se o valor da coluna [0] + o valor da coluna que está passando for menor que o valor da linha que está passando e coluna [0]
-               // Atualiza a primeira coluna da matriz, que será utilizado para as próximas iterações
-               if( (matriz[min][0] + matriz[min][j]) < matriz[j][0] ){
-                matriz[j][0] = matriz[min][0] + matriz[min][j];
+               // If the value of column [0] + the value of the passing column is less than the value of the passing row and column [0]
+               // Update the first column of the matrix, which will be used for the next iterations
+               if( (matrix[min][0] + matrix[min][j]) < matrix[j][0] ){
+                matrix[j][0] = matrix[min][0] + matrix[min][j];
                }
           }
     }
@@ -72,24 +72,24 @@ void Dijkstra(unsigned long int matriz[nroVertices][nroVertices], int n){
 
 int main(){
 
-    unsigned long int Matriz[nroVertices][nroVertices] = {{          0,          6,         10, 4294967295, 4294967295 },
+    unsigned long int Matrix[noVertices][noVertices] = {{          0,          6,         10, 4294967295, 4294967295 },
                                                           {          6,          0, 4294967295,          2, 4294967295 },
                                                           {         10, 4294967295,          0,          1,          3 },
                                                           { 4294967295,          2,          1,          0,          8 },
                                                           { 4294967295, 4294967295,          3,          8,          0 }};
 
-    Dijkstra(Matriz, nroVertices);
+    Dijkstra(Matrix, noVertices);
 
-    printf("Total caminho mais curto do vertice 0 ao 4: %lu\n", Matriz[4][0]); // Caminho total mais curto
+    printf("Total shortest path from vertex 0 to 4: %lu\n", Matrix[4][0]); // Shortest total path
 
-    // Da print na matriz com os valores atualizados
-    printf("Matriz:\n");
-    for (int i = 0; i < nroVertices; ++i){
-        for (int e = 0; e < nroVertices; ++e){
-            if( Matriz[i][e] < 10 )
-                printf(" %lu ", Matriz[i][e]);
+    // Print the matrix with the updated values
+    printf("Matrix:\n");
+    for (int i = 0; i < noVertices; ++i){
+        for (int e = 0; e < noVertices; ++e){
+            if( Matrix[i][e] < 10 )
+                printf(" %lu ", Matrix[i][e]);
             else
-                printf("%lu ", Matriz[i][e]);
+                printf("%lu ", Matrix[i][e]);
         }
         printf("\n");
     }
