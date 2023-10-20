fdef617c31016b5f6d74dac1a327f956654c78cc
Khushi <parikh.3@iitj.ac.in>
2023-10-20 10:12:29 +0530

    changed language of bubble_sort.py from portugese to english

--- src/python/bubble_sort.py
+++ src/python/bubble_sort.py
@@ -1,15 +1,22 @@
-""" Implementação do algoritmo bubble sort com recursão """
+""" 
+Implementação do algoritmo bubble sort com recursão
+Implementation of the bubble sort algorithm with recursion 
+"""
 
 
 def bubble_sort(data, size):
     """
     Implementação de um algoritmo de bubble sort com recursão.
-
     Argumentos:
     data: lista. Lista que será ordenada
     size: int. Tamanho da lista
-
     Retorna a lista "data" ordenada.
+
+    Implementation of a bubble sort algorithm with recursion.
+    Arguments:
+    data: list. List to be sorted
+    size: int. List size
+    Returns the ordered "date" list.
     """
     swap = False
     for i in range(0, size - 1):
@@ -21,7 +28,7 @@ def bubble_sort(data, size):
 
 
 if __name__ == "__main__":
-    lista_nao_ordenada = [2, 9, 8, 0, 1, 3, 5, 4, 6, 7]
-    print(lista_nao_ordenada)
-    bubble_sort(lista_nao_ordenada, len(lista_nao_ordenada))
-    print(lista_nao_ordenada)
+    unordered_list = [2, 9, 8, 0, 1, 3, 5, 4, 6, 7]
+    print(unordered_list)
+    bubble_sort(unordered_list, len(unordered_list))
+    print(unordered_list)
