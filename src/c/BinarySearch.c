91b0f6ad8c8a06688b9a232a6b517a5f48c12c34
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-22 10:46:28 -0300

    Update BinarySearch.c

--- src/c/BinarySearch.c
+++ src/c/BinarySearch.c
@@ -1,46 +1,43 @@
 #include<stdio.h>
-#define SIZE 100
 
-int BinarySearch(int array[], int size, int searched)
-{
-	int start, middle, end;
-	start = 0;
-	end = size - 1;
-	middle = end / 2;
-	
-	while (start < end && array[middle] != searched) 
-	{
-		if (searched > array[middle]) // new start
-			start = middle + 1;
-			
-		if (searched < array[middle]) // new end
-			end = middle - 1;
-			
-		// new middle
-		middle = (start + end) / 2;
-	}
-	
-	if (array[middle] == searched)
-		return middle;
-	
-	return -1;
+int BinarySearch(int array[], int size, int value) {
+    int start = 0;
+    int end = size - 1;
+    int middle = end / 2;
+
+    while (start < end && array[middle] != value) {
+        // new start
+        if (value > array[middle])
+            start = middle + 1;
+
+        // new end
+        if (value < array[middle])
+            end = middle - 1;
+
+        // new middle
+        middle = (start + end) / 2;
+    }
+
+    if (array[middle] == value)
+        return middle;
+
+    return -1;
 }
 
-int main()
-{
-	int searche;
-	int arraySize = 10;
-    int array[SIZE] = {1, 5, 10, 12, 18, 22, 87, 90, 112, 129};
-    
-    printf("Please provide the number you want to search for: ");
-    scanf("%d", &searche);
-    
-    int pos =  BinarySearch(array, arraySize, searche);
-    
+int main() {
+    int value;
+    int array[] = {1, 5, 10, 12, 18, 22, 87, 90, 112, 129};
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Please provide the number you want to value for: ");
+    scanf("%d", &value);
+
+    int pos = BinarySearch(array, size, value);
+
     if (pos != -1)
-    	printf("Found in position = %d.\nValue = %d\n", pos, array[pos]);
-    else 
-    	printf("Not found\n");
-    
+        printf("Found in position = %d.\nValue = %d\n", pos, array[pos]);
+    else
+        printf("Not found\n");
+
     return 0;
 }
