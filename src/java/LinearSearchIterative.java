35979f93622deb52b2c92922455dba6ce683e979
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-24 20:24:45 -0300

    Update LinearSearchIterative.java

--- src/java/LinearSearchIterative.java
+++ src/java/LinearSearchIterative.java
@@ -1,8 +1,8 @@
 public class LinearSearchIterative {
   public static void main(String[] args) {
-    int array_1[] = {1, 2, 40, 233, 78, 83, 456};
-    System.out.println(LinearSearch(array_1, 40));
-    System.out.println(LinearSearch(array_1, 3));
+    int array[] = {1, 2, 40, 233, 78, 83, 456};
+    System.out.println(LinearSearch(array, 40));
+    System.out.println(LinearSearch(array, 3));
   }
 
   public static int LinearSearch(int array[], int value) {
