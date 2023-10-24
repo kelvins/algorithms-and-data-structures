c00c999c9ae42be56e0ff7a7b346a235c7e9da2b
pluto-tofu <paarthqwerty@gmail.com>
2023-10-24 21:55:18 +0530

    tried reformatting

--- src/java/LinearSearchIterative.java
+++ src/java/LinearSearchIterative.java
@@ -1,6 +1,6 @@
 public class LinearSearchIterative {
     public static void main(String[] args) {
-        int array_1[] = { 1, 2, 40, 233, 78, 83, 456 };
+        int array_1[] = {1, 2, 40, 233, 78, 83, 456};
         System.out.println(LinearSearch(array_1, 40));
         System.out.println(LinearSearch(array_1, 3));
     }
