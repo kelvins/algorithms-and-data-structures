145dce3eb4ddc6703e7505238604004a0d551d55
vijaykrishna-11 <vijaykrishna21135@gmail.com>
2023-10-31 12:59:49 +0530

    Added Counting Sort Algorithm

--- README.md
+++ README.md
@@ -2923,8 +2923,8 @@ In order to achieve greater coverage and encourage more people to contribute to 
                 </a>
             </td>
             <td> <!-- Java -->
-                <a href="./CONTRIBUTING.md">
-                    <img align="center" height="25" src="./logos/github.svg" />
+                <a href="./src/java/CountingSort.java">
+                    <img align="center" height="25" src="./logos/java.svg" />
                 </a>
             </td>
             <td> <!-- Python -->
---
+++ src/java/CountingSort.java
@@ -0,0 +1,57 @@
+public class CountingSort {
+
+    int getMax(int[] a, int n) {
+        int max = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] > max)
+                max = a[i];
+        }
+        return max;
+    }
+
+    void countSort(int[] a, int n) {
+        int[] output = new int[n + 1];
+        int max = getMax(a, n);
+        // int max = 42;
+        int[] count = new int[max + 1];
+
+        for (int i = 0; i <= max; ++i) {
+            count[i] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            count[a[i]]++;
+        }
+
+        for (int i = 1; i <= max; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[a[i]] - 1] = a[i];
+            count[a[i]]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            a[i] = output[i];
+        }
+    }
+
+    /* Function to print the array elements */
+    void printArray(int a[], int n) {
+        int i;
+        for (i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+    }
+
+    public static void main(String args[]) {
+        int a[] = { 11, 30, 24, 7, 31, 16, 39, 41 };
+        int n = a.length;
+        CountingSort c1 = new CountingSort();
+        System.out.println("\nBefore sorting array elements are - ");
+        c1.printArray(a, n);
+        c1.countSort(a, n);
+        System.out.println("\nAfter sorting array elements are - ");
+        c1.printArray(a, n);
+        System.out.println();
+    }
+}
