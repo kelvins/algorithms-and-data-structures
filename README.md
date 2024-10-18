01d67edc71625a7ceb0d5f3c2ddb6924d38a3885
Kelvin Prado <kelvin.prado@nubank.com.br>
2024-10-18 15:46:45 -0300

    Add bogosort in Scala

--- README.md
+++ README.md
@@ -3248,8 +3248,8 @@ In order to achieve greater coverage and encourage more people to contribute to 
                 </a>
             </td>
             <td> <!-- Scala -->
-                <a href="./CONTRIBUTING.md">
-                    <img align="center" height="25" src="./logos/github.svg" />
+                <a href="./src/scala/Bogosort.scala">
+                    <img align="center" height="25" src="./logos/scala.svg" />
                 </a>
             </td>
             <td> <!-- Kotlin -->
---
+++ src/scala/Bogosort.scala
@@ -0,0 +1,25 @@
+import scala.annotation.tailrec
+import scala.util.Random
+
+
+@tailrec
+def isSorted(data: Seq[Int]): Boolean = {
+    if (data.size < 2) true
+    else if (data(0) > data(1)) false
+    else isSorted(data.tail)
+}
+
+
+@tailrec
+def bogosort(data: Seq[Int]): Seq[Int] = {
+    val result: Seq[Int] = Random.shuffle(data)
+    if (isSorted(result)) result
+    else bogosort(data)
+}
+
+
+object Main extends App {
+    val data: Seq[Int] = Seq.fill(10)(Random.nextInt(10))
+    println(s"Unsorted data: $data")
+    println(s"Sorted data: ${bogosort(data)}")
+}
