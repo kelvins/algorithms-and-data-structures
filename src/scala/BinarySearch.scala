e71ecf870f41c5009a27c41c7e0490a53828994a
Kelvin Prado <kelvin.prado@nubank.com.br>
2024-10-21 15:54:44 -0300

    Try to fix BinarySearch.scala

--- src/scala/BinarySearch.scala
+++ src/scala/BinarySearch.scala
@@ -12,13 +12,12 @@ def binarySearch(data: Seq[Int], target: Int): Option[Int] = {
       else search(left, middle - 1)
     }
   }
+
   search(0, data.size)
 }
 
 object Main extends App {
   val data: Seq[Int] = Seq(0, 1, 3, 5, 6, 7, 8, 9, 10, 11, 12)
   val value: Int = 11
-  println(
-    s"Value '$value' found in position '${binarySearch(data, value).get}'"
-  )
+  println(s"Value '$value' found in position '${binarySearch(data, value).get}'")
 }
