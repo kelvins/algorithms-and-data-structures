22742fbde249d6319d0350a3bd888822cd624ec4
Kelvin Prado <kelvin.prado@nubank.com.br>
2024-10-21 16:07:03 -0300

    Try to fix BinarySearch.scala

--- src/scala/BinarySearch.scala
+++ src/scala/BinarySearch.scala
@@ -4,9 +4,9 @@ def binarySearch(data: Seq[Int], target: Int): Option[Int] = {
 
   @tailrec
   def search(left: Int, right: Int): Option[Int] = {
-
-    if (left > right) None
-    else {
+    if (left > right) {
+      None
+    } else {
       val middle: Int = (left + right) / 2
       if (data(middle) == target) Some(middle)
       else if (data(middle) < target) search(middle + 1, right)
@@ -18,7 +18,6 @@ def binarySearch(data: Seq[Int], target: Int): Option[Int] = {
 }
 
 object Main extends App {
-
   val data: Seq[Int] = Seq(0, 1, 3, 5, 6, 7, 8, 9, 10, 11, 12)
   val value: Int = 11
   println(s"Value '$value' found in position '${binarySearch(data, value).get}'")
