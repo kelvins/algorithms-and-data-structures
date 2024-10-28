fdb358ecd32f5a2f981c7f1a5194e9e7b396cebb
Paul Sasieta Arana <paul.sasieta@macropay.com>
2024-10-28 07:09:20 +0100

    Linting fixed

--- src/scala/Exponentiation.scala
+++ src/scala/Exponentiation.scala
@@ -1,9 +1,9 @@
 def exponentiation(base: Int, exponent: Int): Int = {
-    (1 to exponent)
-    .map(_ => base)
-    .reduce(_ * _)
+  (1 to exponent)
+  .map(_ => base)
+  .reduce(_ * _)
 }
     
 object Main extends App {
-    println("5 ^ 3 = " + exponentiation(5, 3));
+  println("5 ^ 3 = " + exponentiation(5, 3));
 }
