5b17ea076179f25bf6a8c6edcc90c1832c534c39
Paul Sasieta Arana <paul.sasieta@macropay.com>
2024-10-29 12:45:21 +0100

    Linting fixed

--- src/scala/ExponentiationRecursive.scala
+++ src/scala/ExponentiationRecursive.scala
@@ -1,7 +1,11 @@
 import scala.annotation.tailrec
 
 @tailrec
-def exponentiationRecursive(base: Int, exponent: Int, accumulator: Int = 1): Int = exponent match {
+def exponentiationRecursive(
+    base: Int,
+    exponent: Int,
+    accumulator: Int = 1
+): Int = exponent match {
   case 0 => accumulator
   case _ => exponentiationRecursive(base, exponent - 1, accumulator * base)
 }
