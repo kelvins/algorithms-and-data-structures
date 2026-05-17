6d981da76af26fd253f49c5a9c38c835927863b1
Luka <ngcct4gfrv@privaterelay.appleid.com>
2026-05-17 19:47:23 +0800

    Translate Portuguese comments to English in BubbleSort.c (resolves #181)

--- src/c/BubbleSort.c
+++ src/c/BubbleSort.c
@@ -22,7 +22,7 @@ int main() {
 
   bubble_sort(array, array_size);
 
-  printf("Lista ordenada:\n");
+  printf("Sorted list:\n");
   for (int i = 0; i < array_size - 1; i++)
     printf("%d, ", array[i]);
 
